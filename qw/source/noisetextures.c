2829f8b493fd2c0dfc601be5ce6b8b36420b81c4
Bill Currie <bill@taniwha.org>
2001-04-10 02:16:50 +0000

    include config.h and string[s].h

--- qw/source/noisetextures.c
+++ qw/source/noisetextures.c
@@ -30,6 +30,15 @@
 	$Id$
 */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
 #include <stdlib.h>
 
 #include "QF/mathlib.h"
