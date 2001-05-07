e943e13eb2ab3acee1cfea4b87e8e9ebc97eccd1
Jeff Teunissen <deek@d2dc.net>
2001-05-07 09:44:17 +0000

    Autoconf support for Forge. It's incomplete, but it's mostly working.

---
+++ tools/Forge/acconfig.h
@@ -0,0 +1,15 @@
+/* Headers/Config.h.  Generated automatically by configure.  */
+/* Headers/Config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Name of package */
+#undef PACKAGE
+
+/* Version number of package */
+#undef VERSION
+
