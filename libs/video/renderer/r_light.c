be7307faa97df864e44f84bf0264f2324579f8d6
Bill Currie <bill@taniwha.org>
2001-09-06 03:53:13 +0000

    move the dlight and lightmap code and data from r_main to r_light

--- libs/video/renderer/r_light.c
+++ libs/video/renderer/r_light.c
@@ -46,6 +46,8 @@
 #include "r_local.h"
 #include "r_shared.h"
 
+dlight_t    r_dlights[MAX_DLIGHTS];
+lightstyle_t r_lightstyle[MAX_LIGHTSTYLES];
 
 void
 R_AnimateLight (void)
@@ -403,4 +405,62 @@ R_LightPoint (vec3_t p)
 		r = 0;
 
 	return r;
+}
+
+dlight_t *
+R_AllocDlight (int key)
+{
+	int         i;
+	dlight_t   *dl;
+
+	// first look for an exact key match
+	if (key) {
+		dl = r_dlights;
+		for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
+			if (dl->key == key) {
+				memset (dl, 0, sizeof (*dl));
+				dl->key = key;
+				dl->color[0] = dl->color[1] = dl->color[2] = 1;
+				return dl;
+			}
+		}
+	}
+	// then look for anything else
+	dl = r_dlights;
+	for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
+		if (dl->die < r_realtime) {
+			memset (dl, 0, sizeof (*dl));
+			dl->key = key;
+			dl->color[0] = dl->color[1] = dl->color[2] = 1;
+			return dl;
+		}
+	}
+
+	dl = &r_dlights[0];
+	memset (dl, 0, sizeof (*dl));
+	dl->key = key;
+	return dl;
+}
+
+void
+R_DecayLights (double frametime)
+{
+	int         i;
+	dlight_t   *dl;
+
+	dl = r_dlights;
+	for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
+		if (dl->die < r_realtime || !dl->radius)
+			continue;
+
+		dl->radius -= frametime * dl->decay;
+		if (dl->radius < 0)
+			dl->radius = 0;
+	}
+}
+
+void
+R_ClearDlights (void)
+{
+	memset (r_dlights, 0, sizeof (r_dlights));
 }
--- libs/video/renderer/r_main.c
+++ libs/video/renderer/r_main.c
@@ -21,10 +21,8 @@ qboolean    r_force_fullscreen;
 qboolean    r_paused;
 double      r_realtime;
 double      r_frametime;
-dlight_t    r_dlights[MAX_DLIGHTS];
 entity_t   *r_view_model;
 entity_t   *r_player_entity;
-lightstyle_t r_lightstyle[MAX_LIGHTSTYLES];
 int         r_lineadj;
 qboolean    r_active;
 float       r_time1;
@@ -34,64 +32,6 @@ byte        color_black[4] = { 0, 0, 0, 0 };		// alpha will be explicitly set
 
 fire_t r_fires[MAX_FIRES];
 
-
-dlight_t *
-R_AllocDlight (int key)
-{
-	int         i;
-	dlight_t   *dl;
-
-	// first look for an exact key match
-	if (key) {
-		dl = r_dlights;
-		for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
-			if (dl->key == key) {
-				memset (dl, 0, sizeof (*dl));
-				dl->key = key;
-				dl->color[0] = dl->color[1] = dl->color[2] = 1;
-				return dl;
-			}
-		}
-	}
-	// then look for anything else
-	dl = r_dlights;
-	for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
-		if (dl->die < r_realtime) {
-			memset (dl, 0, sizeof (*dl));
-			dl->key = key;
-			dl->color[0] = dl->color[1] = dl->color[2] = 1;
-			return dl;
-		}
-	}
-
-	dl = &r_dlights[0];
-	memset (dl, 0, sizeof (*dl));
-	dl->key = key;
-	return dl;
-}
-
-void
-R_DecayLights (double frametime)
-{
-	int         i;
-	dlight_t   *dl;
-
-	dl = r_dlights;
-	for (i = 0; i < MAX_DLIGHTS; i++, dl++) {
-		if (dl->die < r_realtime || !dl->radius)
-			continue;
-
-		dl->radius -= frametime * dl->decay;
-		if (dl->radius < 0)
-			dl->radius = 0;
-	}
-}
-
-void
-R_ClearDlights (void)
-{
-	memset (r_dlights, 0, sizeof (r_dlights));
-}
 
 void
 R_ClearFires (void)
