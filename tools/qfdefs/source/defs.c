bcd54926b49175e3d4a4ffd146c1d40fb30ba0c2
Bill Currie <bill@taniwha.org>
2001-09-28 03:58:51 +0000

    make compile with gcc 3.0

--- tools/qfdefs/source/defs.c
+++ tools/qfdefs/source/defs.c
@@ -1,3 +1,12 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#ifdef HAVE_STRING_H
+# include <string.h>
+#endif
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
 #include <QF/hash.h>
 
 #include "def.h"
