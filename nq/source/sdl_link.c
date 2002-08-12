ff02dfbbbc6a5e5ac7d7167a020b7a4788625adf
Bill Currie <bill@taniwha.org>
2002-08-12 16:42:26 +0000

    make the sdl clients link reliably (some versions of binutils don't cope well with no .o files)

---
+++ nq/source/sdl_link.c
@@ -0,0 +1,35 @@
+/*
+	sdl_main.c
+
+	hack to force linking of sdl clients
+
+	Copyright (C) 2002 Bill Currie <bill@taniwha.org>
+
+	Author: Bill Currie <bill@taniwha.org>
+	Date: 2002/8/12
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+*/
+static const char rcsid[] =
+	"$Id$";
+
+extern int noconinput;
+
+static int *const _noconinput = &noconinput;
