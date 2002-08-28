7cc9f482076f71b835eb60115f09115125a792fb
Brian Koropoff <brianhk@cs.washington.edu>
2002-08-28 08:28:05 +0000

    Whoops.  I broke looping under some circumstances in GIB.  Fixed.

--- libs/util/gib_parse.c
+++ libs/util/gib_parse.c
@@ -241,8 +241,12 @@ GIB_Parse_Extract_Line (struct cbuf_s *cbuf)
 		dstring_snip (dstr, 0, i + (dstr->str[i] == '\n' || dstr->str[i] == ';'));
 	}
 
-	return;
+	// If this is a looping buffer and it is now empty,
+	// copy the loop program back in
+	if (GIB_DATA(cbuf)->type == GIB_BUFFER_LOOP && !cbuf->buf->str[0])
+		Cbuf_AddText (cbuf, GIB_DATA(cbuf)->loop_program->str);
 
+	return;
 }
 
 /*
@@ -483,11 +487,5 @@ void GIB_Parse_Execute_Line (cbuf_t *cbuf)
 	} else	
 		Cmd_Command (cbuf->args);
 	dstring_clearstr (cbuf->line);
-	
-	// If this is a looping buffer and it is now empty,
-	// copy the loop program back in and execute any
-	// loop callbacks
-	if (GIB_DATA(cbuf)->type == GIB_BUFFER_LOOP && !cbuf->buf->str[0])
-		Cbuf_AddText (cbuf, GIB_DATA(cbuf)->loop_program->str);
 }
 
