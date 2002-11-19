ac8acaacd7ed617a5a2292f59b35e4ffc00f323e
Bill Currie <bill@taniwha.org>
2002-11-19 05:02:57 +0000

    need limits.h

--- tools/bsp2img/bsp2img.c
+++ tools/bsp2img/bsp2img.c
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <math.h>
 #include <values.h>
 #include <stdint.h>
+#include <limits.h>
 
 #include "QF/bspfile.h"
 #include "QF/cmd.h"
