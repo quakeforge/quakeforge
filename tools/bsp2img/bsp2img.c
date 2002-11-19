599b51b542c990f5623938ce61308462084c8615
Bill Currie <bill@taniwha.org>
2002-11-19 05:03:42 +0000

    hmm, hadn't noticed "stdint.h"

--- tools/bsp2img/bsp2img.c
+++ tools/bsp2img/bsp2img.c
@@ -27,7 +27,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <string.h>
 #include <math.h>
 #include <values.h>
-#include <stdint.h>
 #include <limits.h>
 
 #include "QF/bspfile.h"
