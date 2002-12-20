122b0d81e76a2da5d5e659e6bd2264515374e07d
Bill Currie <bill@taniwha.org>
2002-12-20 23:41:24 +0000

    values.h doesn't seem to be needed and doesn't exist on darwin anyway

--- tools/bsp2img/bsp2img.c
+++ tools/bsp2img/bsp2img.c
@@ -26,7 +26,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <errno.h>
 #include <string.h>
 #include <math.h>
-#include <values.h>
 #include <limits.h>
 
 #include "QF/bspfile.h"
