79322d64ce1c627d2bc4fa3e300a3f09fb1cb58e
Bill Currie <bill@taniwha.org>
2003-02-02 03:43:49 +0000

    header stuff

--- include/snd_render.h
+++ include/snd_render.h
@@ -1,3 +1,36 @@
+/*
+	snd_render.h
+
+	Sound renderer plugin stuff
+
+	Copyright (C) 2002 Bill Currie
+
+	Author: Bill Currie <bill@taniwha.org>
+	Date: Jan 31 2003
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to:
+
+		Free Software Foundation, Inc.
+		59 Temple Place - Suite 330
+		Boston, MA  02111-1307, USA
+
+	$Id$
+*/
+
+#ifndef __snd_render_h
+#define __snd_render_h
 
 // !!! if this is changed, it must be changed in asm_i386.h too !!!
 typedef struct
@@ -96,3 +129,5 @@ extern	channel_t   channels[MAX_CHANNELS];
 // MAX_DYNAMIC_CHANNELS + NUM_AMBIENTS to total_channels = static sounds
 
 extern	int			total_channels;
+
+#endif//__snd_render_h
