b64f193d56968feedb69879970c027f2a174c425
Ragnvald Maartmann-Moe IV <despair@users.sourceforge.net>
2003-03-03 05:34:26 +0000

    Rest of the Write* update.

--- ruamoko/lib/message.r
+++ ruamoko/lib/message.r
@@ -9,4 +9,7 @@ void (float to, float f) WriteCoord = #56;
 void (float to, float f) WriteAngle = #57;
 void (float to, string s) WriteString = #58;
 void (float to, entity s) WriteEntity = #59;
+void (float to, ...) WriteBytes = #0;
+void (float to, vector v) WriteCoordV = #0;
+void (float to, vector v) WriteAngleV = #0;
 void (...) centerprint = #73;
