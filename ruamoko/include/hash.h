27306a9a2be5e18abe54756ce9997686d1cd231d
Bill Currie <bill@taniwha.org>
2003-08-19 21:10:19 +0000

    correct the prototypes for the hash functions (oops:)

--- ruamoko/include/hash.h
+++ ruamoko/include/hash.h
@@ -4,22 +4,22 @@
 struct _hashtab_t = {};
 typedef struct _hashtab_t [] hashtab_t;
 
-@extern hashtab_t () Hash_NewTable;
-@extern void () Hash_SetHashCompare;
-@extern void () Hash_DelTable;
-@extern void () Hash_FlushTable;
-@extern integer () Hash_Add;
-@extern integer () Hash_AddElement;
-@extern (void []) () Hash_Find;
-@extern (void []) () Hash_FindElement;
-@extern (void [][]) () Hash_FindList;
-@extern (void [][]) () Hash_FindElementList;
-@extern (void []) () Hash_Del;
-@extern (void []) () Hash_DelElement;
-@extern void () Hash_Free;
-@extern integer () Hash_String;
-@extern integer () Hash_Buffer;
-@extern (void [][]) () Hash_GetList;
-@extern void () Hash_Stats;
+@extern hashtab_t (integer size, string (void []ele, void []data) gk, void (void []ele, void []data) f, void [] ud) Hash_NewTable;
+@extern void (hashtab_t tab, unsigned (void []ele, void []data) gh, integer (void [] ele1, void [] ele2, void [] data) cmp) Hash_SetHashCompare;
+@extern void (hashtab_t tab) Hash_DelTable;
+@extern void (hashtab_t tab) Hash_FlushTable;
+@extern integer (hashtab_t tab, void [] ele) Hash_Add;
+@extern integer (hashtab_t tab, void [] ele) Hash_AddElement;
+@extern (void []) (hashtab_t tab, string key) Hash_Find;
+@extern (void []) (hashtab_t tab, void [] ele) Hash_FindElement;
+@extern (void [][]) (hashtab_t tab, string key) Hash_FindList;
+@extern (void [][]) (hashtab_t tab, void [] ele) Hash_FindElementList;
+@extern (void []) (hashtab_t tab, string key) Hash_Del;
+@extern (void []) (hashtab_t tab, void [] ele) Hash_DelElement;
+@extern void (hashtab_t tab, void [] ele) Hash_Free;
+@extern integer (string str) Hash_String;
+@extern integer (void [] buf, integer len) Hash_Buffer;
+@extern (void [][]) (hashtab_t tab) Hash_GetList;
+@extern void (hashtab_t tab) Hash_Stats;
 
 #endif	// __ruamoko_hash_h
--- ruamoko/lib/hash.r
+++ ruamoko/lib/hash.r
@@ -1,19 +1,19 @@
 #include "hash.h"
 
-hashtab_t () Hash_NewTable = #0;
-void () Hash_SetHashCompare = #0;
-void () Hash_DelTable = #0;
-void () Hash_FlushTable = #0;
-integer () Hash_Add = #0;
-integer () Hash_AddElement = #0;
-(void []) () Hash_Find = #0;
-(void []) () Hash_FindElement = #0;
-(void [][]) () Hash_FindList = #0;
-(void [][]) () Hash_FindElementList = #0;
-(void []) () Hash_Del = #0;
-(void []) () Hash_DelElement = #0;
-void () Hash_Free = #0;
-integer () Hash_String = #0;
-integer () Hash_Buffer = #0;
-(void [][]) () Hash_GetList = #0;
-void () Hash_Stats = #0;
+@extern hashtab_t (integer size, string (void []ele, void []data) gk, void (void []ele, void []data) f, void [] ud) Hash_NewTable = #0;
+@extern void (hashtab_t tab, unsigned (void []ele, void []data) gh, integer (void [] ele1, void [] ele2, void [] data) cmp) Hash_SetHashCompare = #0;
+@extern void (hashtab_t tab) Hash_DelTable = #0;
+@extern void (hashtab_t tab) Hash_FlushTable = #0;
+@extern integer (hashtab_t tab, void [] ele) Hash_Add = #0;
+@extern integer (hashtab_t tab, void [] ele) Hash_AddElement = #0;
+@extern (void []) (hashtab_t tab, string key) Hash_Find = #0;
+@extern (void []) (hashtab_t tab, void [] ele) Hash_FindElement = #0;
+@extern (void [][]) (hashtab_t tab, string key) Hash_FindList = #0;
+@extern (void [][]) (hashtab_t tab, void [] ele) Hash_FindElementList = #0;
+@extern (void []) (hashtab_t tab, string key) Hash_Del = #0;
+@extern (void []) (hashtab_t tab, void [] ele) Hash_DelElement = #0;
+@extern void (hashtab_t tab, void [] ele) Hash_Free = #0;
+@extern integer (string str) Hash_String = #0;
+@extern integer (void [] buf, integer len) Hash_Buffer = #0;
+@extern (void [][]) (hashtab_t tab) Hash_GetList = #0;
+@extern void (hashtab_t tab) Hash_Stats = #0;
