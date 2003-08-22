a48b2dd1034c8a8a3e45b4628048b6f7f2fbb91d
Bill Currie <bill@taniwha.org>
2003-08-22 06:34:09 +0000

    fix some errors picked up by the recent changes to qfcc

--- ruamoko/game/Axe.r
+++ ruamoko/game/Axe.r
@@ -37,7 +37,7 @@
 
 	org = trace_endpos - v_forward * 4;
 
-	if ([trace_ent.@this takeDamage:self :s :s :damage])
+	if ([trace_ent.@this takeDamage:self :owner :owner :damage])
 		SpawnBlood (org, 20);
 	else {
 		sound (s, CHAN_WEAPON, "player/axhit2.wav", 1, ATTN_NORM);
