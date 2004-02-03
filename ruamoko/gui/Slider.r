0e0f3704c569a9d96e8c0895e2d83fb2db150f8c
Bill Currie <bill@taniwha.org>
2004-02-03 13:33:16 +0000

    correct the math for calculating the position of the slider indicator

--- ruamoko/gui/Slider.r
+++ ruamoko/gui/Slider.r
@@ -27,15 +27,14 @@
 {
 	local integer pos, x, y;
 
+	pos = (index * ((dir ? ylen : xlen) - 24) / size) + 8;
 	if (dir) {
-		pos = index * ylen / size;
 		Draw_Character (xabs, yabs, 1);
 		for (y = 8; y < ylen - 8; y += 8)
 			Draw_Character (xabs, yabs + y, 2);
 		Draw_Character (xabs, yabs + y, 3);
 		Draw_Character (xabs, yabs + pos, 131);
 	} else {
-		pos = index * xlen / size;
 		Draw_Character (xabs, yabs, 128);
 		for (x = 8; x < xlen - 8; x += 8)
 			Draw_Character (xabs + x, yabs, 129);
