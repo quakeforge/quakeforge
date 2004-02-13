e3a393dd52f61962167e7e83e1a0204282c3634e
Bill Currie <bill@taniwha.org>
2004-02-13 02:52:14 +0000

    add Pic and CenterPic views

---
+++ ruamoko/gui/Pic.r
@@ -0,0 +1,33 @@
+#include "draw.h"
+#include "string.h"
+#include "gui/Pic.h"
+
+@implementation Pic
+-(id)init
+{
+	pic_name = str_new ();
+	return self;
+}
+
+-(void)dealloc
+{
+	str_free (pic_name);
+}
+
+-(void)setPic:(string)pic
+{
+	str_copy (pic_name, pic);
+}
+
+-(void)draw
+{
+	Draw_Pic (xabs, yabs, Draw_CachePic (pic_name, 1));
+}
+@end
+
+@implementation CenterPic
+-(void)draw
+{
+	Draw_CenterPic (xabs, yabs, Draw_CachePic (pic_name, 1));
+}
+@end
---
+++ ruamoko/include/gui/Pic.h
@@ -0,0 +1,18 @@
+#ifndef __ruamoko_gui_Pic_h
+#define __ruamoko_gui_Pic_h
+
+#include "gui/View.h"
+
+@interface Pic : View
+{
+	string pic_name;
+}
+-(void)setPic:(string)pic;
+-(void)draw;
+@end
+
+@interface CenterPic : Pic
+-(void)draw;
+@end
+
+#endif//__ruamoko_gui_Pic_h
