9c9b6714bed8a798d17a5b3b387e7e2b8b9d9836
Bill Currie <bill@taniwha.org>
2004-03-21 05:39:59 +0000

    the initial event mask now needs to include mouse motion events otherwise the x11 clients will "lock up" when they start fullscreened

--- include/context_x11.h
+++ include/context_x11.h
@@ -44,7 +44,7 @@
 						| PointerMotionMask)
 #define X11_FOCUS_MASK (FocusChangeMask | EnterWindowMask)
 #define X11_INPUT_MASK (X11_KEY_MASK | X11_MOUSE_MASK | X11_FOCUS_MASK)
-#define X11_MASK (X11_WINDOW_MASK | X11_FOCUS_MASK)
+#define X11_MASK (X11_WINDOW_MASK | X11_FOCUS_MASK | PointerMotionMask)
 
 extern Display	*x_disp;
 extern Visual	*x_vis;
