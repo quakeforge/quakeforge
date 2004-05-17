e3008841be4ed9b4b79c5ffc5f8f03b48695d2b2
Bill Currie <bill@taniwha.org>
2004-05-17 22:01:18 +0000

    forgot net_loopback_adr in the ipv6 code (thanks, SpanKY)

--- libs/net/nc/net_udp6.c
+++ libs/net/nc/net_udp6.c
@@ -123,6 +123,7 @@ static __attribute__ ((unused)) const char rcsid[] =
 
 netadr_t    net_from;
 netadr_t    net_local_adr;
+netadr_t    net_loopback_adr;
 int         net_socket;
 
 static sizebuf_t _net_message_message;
