f9541dcae7d040c96e19e1f99d6acfb9b2dc1fed
Bill Currie <bill@taniwha.org>
2005-05-07 10:46:19 +0000

    fix an old bug that would mess up bots (geez, globals can suck)

--- qw/source/sv_user.c
+++ qw/source/sv_user.c
@@ -370,12 +370,12 @@ SV_Spawn (client_t *client)
 	memset (&ent->v, 0, sv_pr_state.progs->entityfields * 4);
 	SVfloat (ent, colormap) = NUM_FOR_EDICT (&sv_pr_state, ent);
 	SVfloat (ent, team) = 0; // FIXME
-	SVstring (ent, netname) = PR_SetString (&sv_pr_state, host_client->name);
+	SVstring (ent, netname) = PR_SetString (&sv_pr_state, client->name);
 
-	host_client->entgravity = 1.0;
+	client->entgravity = 1.0;
 	if (sv_fields.gravity != -1)
 		SVfloat (ent, gravity) = 1.0;
-	host_client->maxspeed = sv_maxspeed->value;
+	client->maxspeed = sv_maxspeed->value;
 	if (sv_fields.maxspeed != -1)
 		SVfloat (ent, maxspeed) = sv_maxspeed->value;
 }
