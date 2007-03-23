407e362d1ce4c8a4c7187d6f26f47d847ce1e864
Bill Currie <bill@taniwha.org>
2007-03-23 13:39:02 +0000

    patch from phrosty to fix the borked svn update

--- vc2005/clean.ps1
+++ vc2005/clean.ps1
@@ -1,8 +1,3 @@
-ls -recurse -include 'win32' | rm -recurse
-ls -recurse -include '*.user' | rm -recurse
-if(test-path QuakeForge.suo) { rm QuakeForge.suo -force }
-if(test-path QuakeForge.ncb) { rm QuakeForge.ncb }
-ls -recurse -include 'win32' | rm -recurse
-ls -recurse -include '*.user' | rm -recurse
+ls -recurse -include 'win32','*.user' | rm -recurse
 if(test-path QuakeForge.suo) { rm QuakeForge.suo -force }
 if(test-path QuakeForge.ncb) { rm QuakeForge.ncb }
