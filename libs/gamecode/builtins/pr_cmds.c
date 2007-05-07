beb5438484027c162af92adf4460d03eac75de80
Bill Currie <bill@taniwha.org>
2007-05-07 13:02:35 +0000

    fix messed up build order by putting bi_inputline.c where it really belongs

--- libs/gamecode/builtins/pr_cmds.c
+++ libs/gamecode/builtins/pr_cmds.c
@@ -55,7 +55,6 @@ static __attribute__ ((used)) const char rcsid[] =
 #include "QF/csqc.h"
 #define U __attribute__ ((used))
 static U void (*const gib_progs_init)(progs_t *) = GIB_Progs_Init;
-static U void (*const inputline_progs_init)(progs_t *) = InputLine_Progs_Init;
 #undef U
 
 VISIBLE const char *pr_gametype = "";
