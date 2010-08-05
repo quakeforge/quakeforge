e9a1e5552e1f8eb486abaf5393001e6b23524452
Bill Currie <bill@taniwha.org>
2010-08-05 03:57:48 +0000

    send multiple almost empty packets when a server frame is too long

--- qw/source/sv_recorder.c
+++ qw/source/sv_recorder.c
@@ -165,8 +165,17 @@ write_msg (sizebuf_t *msg, int type, int to, float time, sizebuf_t *dst)
 
 	msec = (time - prevtime) * 1000;
 	prevtime += msec * 0.001;
-	if (msec > 255)
-		msec = 255;
+	while (msec > 250) {
+		//the maximum interval we can send is 255ms, but that's nasty with,
+		//floats, so keep things to a nice round number of 250ms (0.25 is float
+		//friendly)
+		MSG_WriteByte (dst, 250);
+		MSG_WriteByte (dst, dem_read);
+		MSG_WriteLong (dst, -1);
+		MSG_WriteByte (dst, A2C_PRINT);
+		MSG_WriteString (dst, "");
+		msec -= 250;
+	}
 	if (msec < 2)
 		msec = 0;
 
