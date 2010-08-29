6c89883c8fefdde67956a0b7b60c5fc509693546
Bill Currie <bill@taniwha.org>
2010-08-29 18:31:04 +0900

    Lots of csg4 documentation

--- tools/qfbsp/include/csg4.h
+++ tools/qfbsp/include/csg4.h
@@ -30,12 +30,53 @@ struct visfacet_s;
 struct brushset_s;
 struct surface_s;
 
-// build surfaces is also used by GatherNodeFaces
 extern struct visfacet_s *validfaces[MAX_MAP_PLANES];
+
+/**	Build the surface lists for the brushset.
+
+	Each plane still with faces after clipping will have one surface, and
+	each surface will list the faces on that plane.
+
+	\return			Chain of all the external surfaces with one or more
+					visible faces.
+*/
 struct surface_s *BuildSurfaces (void);
 
+/**	Create a duplicate face.
+
+	Duplicates the non point information of a face.
+
+	\param in		The face to duplicate.
+	\return			The new face.
+
+*/
 struct visfacet_s *NewFaceFromFace (struct visfacet_s *in);
+
+/**	Convert a brush-set to a list of clipped faces.
+
+	The brushes of the brush set will be clipped against each other.
+
+	\param bs		The brush-set to convert.
+	\return			list of surfaces containing all of the faces
+*/
 struct surface_s *CSGFaces (struct brushset_s *bs);
+
+/**	Split a face by a plane.
+
+	\param in		The face to split.
+	\param split	The plane by which to split the face.
+	\param front	Set to the new face repesenting the part of the input
+					face that is in front of the plane, or NULL if there is
+					no front portion.
+	\param back		Set to the new face repesenting the part of the input
+					face that is behind the plane, or NULL if there is
+					no back portion.
+	\note A face entirely on the plane is considered to be behind the plane.
+
+	\note The face represented by \a in becomes invalid. If either \a front
+	or \a back is NULL, then no split has occurred and the other will be set
+	to \a in.
+*/
 void SplitFace (struct visfacet_s *in, struct plane_s *split,
 				struct visfacet_s **front, struct visfacet_s **back);
 
