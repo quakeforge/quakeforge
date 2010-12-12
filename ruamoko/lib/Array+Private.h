f13c45a9ae4876456d459f1c8b5a8f548cfc1573
Jeff Teunissen <deek@d2dc.net>
2010-12-11 23:45:48 -0500

    kill extra reference to Array in docs

--- ruamoko/lib/Array+Private.h
+++ ruamoko/lib/Array+Private.h
@@ -12,7 +12,7 @@
 	Adds an object to the receiver, but without retaining it.
 
 	This is a dangerous thing to do, and it's only done so that we can use an 
-	Array to implement the AutoreleasePool class.
+	%Array to implement the AutoreleasePool class.
 
 	\warning Using this method can result in crashes, and is only included 
 	for internal use by other classes.
