775f8e959b534d10785ac074c5d17c83bf3a2195
Bill Currie <bill@taniwha.org>
2010-12-15 08:33:20 +0900

    Rotating bsp model support.
    This is based on the code presented on
    http://forums.inside3d.com/viewtopic.php?t=2376 (and updates linked from
    that thread), but with modifications for hopefully supporting rotating
    trains (and not duplicating big fuctions).
    
    For now, only nq. qw will come after some more testing.

--- nq/source/sv_phys.c
+++ nq/source/sv_phys.c
@@ -397,24 +397,35 @@ SV_PushEntity (edict_t *ent, vec3_t push)
 }
 
 static qboolean
-SV_Push (edict_t *pusher, vec3_t move)
+SV_Push (edict_t *pusher, vec3_t tmove, vec3_t amove)
 {
 	float       solid_save;
 	int         num_moved, i, e;
 	edict_t    *check, *block;
 	edict_t   **moved_edict;
-	vec3_t      entorig;
-	vec3_t      mins, maxs, pushorig;
+	vec3_t      entorig, move, org, org2;
+	vec3_t      mins, maxs, pushtorig, pushaorig;
 	vec3_t     *moved_from;
+	vec3_t      forward = {1,  0, 0};
+	vec3_t      right   = {0, -1, 0};
+	vec3_t      up      = {0,  0, 1};
 	int         mark;
 
-	VectorAdd (SVvector (pusher, absmin), move, mins);
-	VectorAdd (SVvector (pusher, absmax), move, maxs);
+	VectorAdd (SVvector (pusher, absmin), tmove, mins);
+	VectorAdd (SVvector (pusher, absmax), tmove, maxs);
 
-	VectorCopy (SVvector (pusher, origin), pushorig);
+	if (!VectorIsZero (amove)) {
+		vec3_t      a;
+		VectorSubtract (vec3_origin, amove, a);
+		AngleVectors (a, forward, right, up);
+	}
+
+	VectorCopy (SVvector (pusher, origin), pushtorig);
+	VectorCopy (SVvector (pusher, angles), pushaorig);
 
 	// move the pusher to it's final position
-	VectorAdd (SVvector (pusher, origin), move, SVvector (pusher, origin));
+	VectorAdd (SVvector (pusher, origin), tmove, SVvector (pusher, origin));
+	VectorAdd (SVvector (pusher, angles), amove, SVvector (pusher, angles));
 	SV_LinkEdict (pusher, false);
 
 	mark = Hunk_LowMark ();
@@ -460,6 +471,15 @@ SV_Push (edict_t *pusher, vec3_t move)
 		moved_edict[num_moved] = check;
 		num_moved++;
 
+		// calculate destination position
+		VectorSubtract (SVvector (check, origin),
+						SVvector (pusher, origin), org);
+		org2[0] = DotProduct (org, forward);
+		org2[1] = -DotProduct (org, right);
+		org2[2] = DotProduct (org, up);
+		VectorSubtract (org2, org, move);
+		VectorAdd (move, tmove, move);
+
 		// try moving the contacted entity
 		solid_save = SVfloat (pusher, solid);
 		SVfloat (pusher, solid) = SOLID_NOT;
@@ -468,6 +488,8 @@ SV_Push (edict_t *pusher, vec3_t move)
 
 		block = SV_TestEntityPosition (check);
 		if (!block) {
+			VectorAdd (SVvector (check, angles), amove,
+					   SVvector (check, angles));
 			continue;
 		}
 		// if it is still inside the pusher, block
@@ -484,7 +506,8 @@ SV_Push (edict_t *pusher, vec3_t move)
 		VectorCopy (entorig, SVvector (check, origin));
 		SV_LinkEdict (check, true);
 
-		VectorCopy (pushorig, SVvector (pusher, origin));
+		VectorCopy (pushtorig, SVvector (pusher, origin));
+		VectorCopy (pushaorig, SVvector (pusher, angles));
 		SV_LinkEdict (pusher, false);
 
 		// if the pusher has a "blocked" function, call it
@@ -495,6 +518,8 @@ SV_Push (edict_t *pusher, vec3_t move)
 		// move back any entities we already moved
 		for (i = 0; i < num_moved; i++) {
 			VectorCopy (moved_from[i], SVvector (moved_edict[i], origin));
+			VectorSubtract (SVvector (moved_edict[i], angles), amove,
+							SVvector (moved_edict[i], angles));
 			SV_LinkEdict (moved_edict[i], false);
 		}
 		Hunk_FreeToLowMark (mark);
@@ -508,15 +533,18 @@ static void
 SV_PushMove (edict_t *pusher, float movetime)
 {
 	vec3_t      move;
+	vec3_t      amove;
 
-	if (VectorIsZero (SVvector (pusher, velocity))) {
+	if (VectorIsZero (SVvector (pusher, velocity))
+		&& VectorIsZero (SVvector (pusher, avelocity))) {
 		SVfloat (pusher, ltime) += movetime;
 		return;
 	}
 
 	VectorScale (SVvector (pusher, velocity), movetime, move);
+	VectorScale (SVvector (pusher, avelocity), movetime, amove);
 
-	if (SV_Push (pusher, move))
+	if (SV_Push (pusher, move, amove))
 		SVfloat (pusher, ltime) += movetime;
 }
 
