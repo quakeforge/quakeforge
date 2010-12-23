32f7cd8b2248e3998de7f4e14a4331a4ee3b382b
Bill Currie <bill@taniwha.org>
2010-12-23 13:18:18 +0900

    Get HTTP support (libcurl) working with mingw.

--- tools/cross/cross-configure.sh
+++ tools/cross/cross-configure.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/sh -x
 MINGW=/home/bill/src/mingw/mingw-cross-env-2.18
 export PKG_CONFIG_PATH=$MINGW/usr/lib/pkgconfig
 export PATH=$MINGW/usr/bin:$PATH
 ../configure --host=i686-pc-mingw32 \
-	SDL_CONFIG=$MINGW/usr/i686-pc-mingw32/bin/sdl-config $*
+	SDL_CONFIG=$MINGW/usr/i686-pc-mingw32/bin/sdl-config \
+	--with-libcurl=$MINGW/usr/i686-pc-mingw32 \
+	$*
 
