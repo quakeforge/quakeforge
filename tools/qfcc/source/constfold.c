ba27db6b9302b79c699543b66c76e24e6465a7cf
Bill Currie <bill@taniwha.org>
2011-01-09 23:23:54 +0900

    Use some math identities to eliminate operations on non-constants.

--- tools/qfcc/source/constfold.c
+++ tools/qfcc/source/constfold.c
@@ -216,6 +216,19 @@ do_op_float (int op, expr_t *e, expr_t *e1, expr_t *e2)
 	}
 	e->e.expr.type = type;
 
+	if (op == '*' && is_constant (e1) && e1->e.float_val == 1)
+		return e2;
+	if (op == '*' && is_constant (e2) && e2->e.float_val == 1)
+		return e1;
+	if (op == '/' && is_constant (e2) && e2->e.float_val == 1)
+		return e1;
+	if (op == '+' && is_constant (e1) && e1->e.float_val == 0)
+		return e2;
+	if (op == '+' && is_constant (e2) && e2->e.float_val == 0)
+		return e1;
+	if (op == '-' && is_constant (e2) && e2->e.float_val == 0)
+		return e1;
+
 	if (op == '=' || op == 'b' || !is_constant (e1) || !is_constant (e2))
 		return e;
 
@@ -327,6 +340,22 @@ do_op_vector (int op, expr_t *e, expr_t *e1, expr_t *e2)
 	} else {
 		e->e.expr.type = &type_vector;
 	}
+
+	if (op == '*' && is_constant (e1) && e1->type == ex_float
+		&& e1->e.float_val == 1)
+		return e2;
+	if (op == '*' && is_constant (e2) && e2->type == ex_float
+		&& e2->e.float_val == 1)
+		return e1;
+	if (op == '/' && is_constant (e2) && e2->type == ex_float
+		&& e2->e.float_val == 1)
+		return e1;
+	if (op == '+' && is_constant (e1) && VectorIsZero (e1->e.vector_val))
+		return e2;
+	if (op == '+' && is_constant (e2) && VectorIsZero (e2->e.vector_val))
+		return e1;
+	if (op == '-' && is_constant (e2) && VectorIsZero (e2->e.vector_val))
+		return e1;
 
 	if (op == '=' || op == 'b' || !is_constant (e1) || !is_constant (e2))
 		return e;
@@ -489,6 +518,22 @@ do_op_quaternion (int op, expr_t *e, expr_t *e1, expr_t *e2)
 		e->e.expr.type = &type_quaternion;
 	}
 
+	if (op == '*' && is_constant (e1) && e1->type == ex_float
+		&& e1->e.float_val == 1)
+		return e2;
+	if (op == '*' && is_constant (e2) && e2->type == ex_float
+		&& e2->e.float_val == 1)
+		return e1;
+	if (op == '/' && is_constant (e2) && e2->type == ex_float
+		&& e2->e.float_val == 1)
+		return e1;
+	if (op == '+' && is_constant (e1) && QuatIsZero (e1->e.vector_val))
+		return e2;
+	if (op == '+' && is_constant (e2) && QuatIsZero (e2->e.vector_val))
+		return e1;
+	if (op == '-' && is_constant (e2) && QuatIsZero (e2->e.vector_val))
+		return e1;
+
 	if (op == '=' || op == 'b' || !is_constant (e1) || !is_constant (e2))
 		return e;
 
@@ -556,6 +601,19 @@ do_op_integer (int op, expr_t *e, expr_t *e1, expr_t *e2)
 	} else {
 		e->e.expr.type = &type_integer;
 	}
+
+	if (op == '*' && is_constant (e1) && e1->e.integer_val == 1)
+		return e2;
+	if (op == '*' && is_constant (e2) && e2->e.integer_val == 1)
+		return e1;
+	if (op == '/' && is_constant (e2) && e2->e.integer_val == 1)
+		return e1;
+	if (op == '+' && is_constant (e1) && e1->e.integer_val == 0)
+		return e2;
+	if (op == '+' && is_constant (e2) && e2->e.integer_val == 0)
+		return e1;
+	if (op == '-' && is_constant (e2) && e2->e.integer_val == 0)
+		return e1;
 
 	if (op == '=' || op == 'b' || !is_constant (e1) || !is_constant (e2))
 		return e;
