b3d57893774056de86bf1e2e2fcf28104c019b6b
Bill Currie <bill@taniwha.org>
2011-02-14 15:46:06 +0900

    More tests.

---
+++ tools/qfcc/test/decl.r
@@ -0,0 +1,5 @@
+integer x;
+integer *y;
+integer *z[4];
+integer (*w)[4];
+integer (foo[3])(string);
---
+++ tools/qfcc/test/syntax.r
@@ -0,0 +1,43 @@
+int z;
+void foo (void)
+{
+	int x;
+	integer y = x;
+}
+
+@interface something
+integer x;
+@end
+
+@implementation something
+integer a;
+void doit (float how)
+{
+	float why;
+}
+
+-hi
+{
+}
+@end
+
+@interface something(other)
+{
+	integer x;
+}
+-hi: (integer) x there:(float) y
+@end
+
+float hah (vod)
+{
+}
+
+float ahd (void)
+{
+	float x;
+}
+
+@protocol whee
+-hi
+-bye;
+@end
