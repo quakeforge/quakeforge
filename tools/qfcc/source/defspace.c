1246fd9e30fb3c340820980cdf7fe9a183f9b500
Bill Currie <bill@taniwha.org>
2011-03-07 13:36:00 +0900

    Use the correct size when growing a data space.

--- tools/qfcc/source/defspace.c
+++ tools/qfcc/source/defspace.c
@@ -70,7 +70,14 @@ static locref_t *free_locrefs;
 static int
 grow_space (defspace_t *space)
 {
-	int         size = space->max_size + GROW;
+	int         size;
+
+	if (space->size >= space->size) {
+		size = space->size + GROW;
+		size -= size % GROW;
+	} else {
+		size = space->max_size + GROW;
+	}
 	space->data = realloc (space->data, size * sizeof (pr_type_t));
 	memset (space->data + space->max_size, 0, GROW * sizeof (pr_type_t));
 	space->max_size = size;
