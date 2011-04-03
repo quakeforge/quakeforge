38b25e0c38448fcc4f22aee678e1dd8f70c762e3
Bill Currie <bill@taniwha.org>
2011-04-03 13:11:50 +0900

    Do not double-allocate space for immediates.
    Caused by changed semantics in def creation.

--- tools/qfcc/source/value.c
+++ tools/qfcc/source/value.c
@@ -304,7 +304,6 @@ emit_value (ex_value_t *value, def_t *def)
 		}
 	} else {
 		cn = new_def (".imm", type, pr.near_data, st_static);
-		cn->offset = defspace_alloc_loc (pr.near_data, type_size (type));
 	}
 	cn->initialized = cn->constant = 1;
 	cn->nosave = 1;
