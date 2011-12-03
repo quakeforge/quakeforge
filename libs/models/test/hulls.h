dadfb7899246907abe71a69c8933d57b03bf1c3f
Bill Currie <bill@taniwha.org>
2011-12-03 20:20:33 +0900

    Split out the hull data so it can be used in other tests.

---
+++ libs/models/test/hulls.h
@@ -0,0 +1,12 @@
+#include "world.h"
+
+extern hull_t hull_simple_wedge;
+extern hull_t hull_tpp1;
+extern hull_t hull_tpp2;
+extern hull_t hull_tppw;
+extern hull_t hull_step1;
+extern hull_t hull_step2;
+extern hull_t hull_step3;
+extern hull_t hull_covered_step;
+extern hull_t hull_ramp;
+extern hull_t hull_hole;
