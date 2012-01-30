e4f63fba6cfb21688028e2a7607c08018143f791
Bill Currie <bill@taniwha.org>
2012-01-30 09:13:06 +0900

    Fix some integer shift overflows.
    Thanks to Ozkan Sezer for pointing them out.

--- libs/video/renderer/sw32/sw32_redge.c
+++ libs/video/renderer/sw32/sw32_redge.c
@@ -480,7 +480,7 @@ R_ScanEdges (void)
 	edge_aftertail.prev = &edge_tail;
 
 	// FIXME: do we need this now that we clamp x in r_draw.c?
-	edge_sentinel.u = 2000 << 24;		// make sure nothing sorts past this
+	edge_sentinel.u = 32767 << 16;		// make sure nothing sorts past this
 	edge_sentinel.prev = &edge_aftertail;
 
 	// process all scan lines
