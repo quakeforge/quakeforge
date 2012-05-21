da8355a5f69eb4012a1777485f87e328b6df44da
Bill Currie <bill@taniwha.org>
2012-05-21 22:26:29 +0900

    Remove some commented code.

--- nq/source/cl_view.c
+++ nq/source/cl_view.c
@@ -101,8 +101,6 @@ V_CalcRoll (const vec3_t angles, const vec3_t velocity)
 	side = fabs (side);
 
 	value = cl_rollangle->value;
-//	if (cl.inwater)
-//		value *= 6;
 
 	if (side < cl_rollspeed->value)
 		side = side * value / cl_rollspeed->value;
