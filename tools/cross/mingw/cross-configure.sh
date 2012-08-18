5684347088b18e62e5650f2f515affc0ba388e50
Bill Currie <bill@taniwha.org>
2012-08-18 22:12:02 +0900

    Some fixes for mingw cross-builds.
    The fixes came to light while getting ps3toolchain cross-building working.

--- tools/cross/mingw/cross-configure.sh
+++ tools/cross/mingw/cross-configure.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -x
 MINGW=/home/bill/src/mingw/mingw-cross-env-2.18
-export PKG_CONFIG_PATH=$MINGW/usr/lib/pkgconfig
+export PKG_CONFIG_LIBDIR=$MINGW/usr/lib/pkgconfig
 export PATH=$MINGW/usr/bin:$PATH
 ../configure --host=i686-pc-mingw32 \
 	SDL_CONFIG=$MINGW/usr/i686-pc-mingw32/bin/sdl-config \
-	--with-libcurl=$MINGW/usr/i686-pc-mingw32 \
 	$*
-
