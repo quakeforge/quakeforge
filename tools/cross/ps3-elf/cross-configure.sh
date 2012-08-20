365f20bb951be2c5b2ca5a40595c857a28a75d12
Bill Currie <bill@taniwha.org>
2012-08-20 10:56:09 +0900

    Make the ps3 cross-build self-contained.
    More or less.
    
    This is a bit of an experiment (which seems to work nicely) in that qfcc
    and pak are build natively in one build tree, then the full system is
    cross-built in another tree using the natively build qfcc and pak. Both
    trees are created by cross-configure.sh as sub-directories of the current
    directory. However, cross-configure.sh still assumes it is being run in a
    subdirectory of the main quakeforge directory.
    
    cross-make.sh checks if the native tree is in the current directory and if
    so, builds it, otherwise it just runs make for the cross-build tree (this
    allows for running cross-make in a sub-directory for things like sorting
    out build issues).

--- tools/cross/ps3-elf/cross-configure.sh
+++ tools/cross/ps3-elf/cross-configure.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -x
+mkdir native powerpc64-ps3-elf
+cd native
+../../configure --without-clients --without-servers --with-tools=qfcc,pak
+cd ../powerpc64-ps3-elf
 export PS3DEV=/usr/local/ps3dev
 export PSL1GHT=$PS3DEV
 export PKG_CONFIG_LIBDIR=$PS3DEV/portlibs/ppu/lib/pkgconfig
 export PATH=$PS3DEV/spu/bin:$PS3DEV/ppu/bin:$PS3DEV/bin:$PATH
-../configure --host=powerpc64-ps3-elf \
+../../configure --host=powerpc64-ps3-elf \
 	--disable-sdl-cd \
 	--with-endian=big \
 	SDL_CONFIG=$PS3DEV/portlibs/ppu/bin/sdl-config \
--- tools/cross/ps3-elf/cross-make.sh
+++ tools/cross/ps3-elf/cross-make.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -x
+if test -d native; then
+	cd native
+	make
+	cd ../powerpc64-ps3-elf
+	ln -fs ../native/tools/qfcc/source/qfcc .
+	ln -fs ../native/tools/pak/pak .
+fi
 export PS3DEV=/usr/local/ps3dev
 export PSL1GHT=$PS3DEV
 export PKG_CONFIG_LIBDIR=$PS3DEV/portlibs/ppu/lib/pkgconfig
 export PATH=$PS3DEV/spu/bin:$PS3DEV/ppu/bin:$PS3DEV/bin:$PATH
 
-make PAK=pak QFCC=qfcc $*
+make PAK='$(top_builddir)/pak' QFCC='$(top_builddir)/qfcc' $*
