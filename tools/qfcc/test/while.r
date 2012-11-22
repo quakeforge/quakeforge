95bd4ad4107c3d316b3068d2b520be50065a276e
Bill Currie <bill@taniwha.org>
2012-11-22 13:29:36 +0900

    Add a test for while (count--)

---
+++ tools/qfcc/test/while.r
@@ -0,0 +1,29 @@
+int min;
+int times;
+
+void bar (int x)
+{
+	if (x < min)
+		min = x;
+}
+
+void foo (int count)
+{
+	int safety = count;	// in case qfcc screws up (prevents infinite loop)
+	min = count;
+	while (count--) {	//XXX this is the code being tested
+		bar (count);
+		times++;
+		safety--;
+		if (safety < 0)
+			break;
+	}
+}
+
+int
+main ()
+{
+	int c = 3;
+	foo (c);
+	return min || times != c;
+}
