62bac6e7b40a27aebd630b605803c46e7dcd32cd
Bill Currie <bill@taniwha.org>
2012-11-27 12:40:43 +0900

    Add scripts for cross-compiling for mipsel-linux.
    These are based on the ps3dev scripts, so native qfcc and pak are built
    automatically.
    
    Note that there may be a need to replace or even just nuke bison in the
    toolchain as it is too old and can't build qfcc.

---
+++ tools/cross/mipsel-linux/cross-configure.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+mkdir native mipsel-linux
+cd native
+../../configure --without-clients --without-servers --with-tools=qfcc,pak
+cd ../mipsel-linux
+export MIPSEL=/opt/gcw0-toolchain
+export PKG_CONFIG_LIBDIR=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot/lib/pkgconfig
+export PATH=$MIPSEL/usr/bin:$PATH
+../../configure --host=mipsel-linux \
+	--disable-sdl-cd \
+	--with-endian=big \
+	SDL_CONFIG=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/sdl-config \
+	$*
---
+++ tools/cross/mipsel-linux/cross-make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+if test -d native; then
+	cd native
+	make
+	cd ../mipsel-linux
+	ln -fs ../native/tools/qfcc/source/qfcc .
+	ln -fs ../native/tools/pak/pak .
+fi
+export MIPSEL=/opt/gcw0-toolchain
+export PKG_CONFIG_LIBDIR=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot/lib/pkgconfig
+export PATH=$MIPSEL/usr/bin:$PATH
+
+make PAK='$(top_builddir)/pak' QFCC='$(top_builddir)/qfcc' $*
