7c3ad46b487e3fea728e25cf0c0f9c4bb93436d3
Bill Currie <bill@taniwha.org>
2012-11-28 19:15:38 +0900

    Correct the endianess.
    mips-linux is little endian (missed edit from the copy from ps3)

--- tools/cross/mipsel-linux/cross-configure.sh
+++ tools/cross/mipsel-linux/cross-configure.sh
@@ -8,6 +8,6 @@ export PKG_CONFIG_LIBDIR=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot/lib/pkgcon
 export PATH=$MIPSEL/usr/bin:$PATH
 ../../configure --host=mipsel-linux \
 	--disable-sdl-cd \
-	--with-endian=big \
+	--with-endian=little \
 	SDL_CONFIG=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/sdl-config \
 	$*
