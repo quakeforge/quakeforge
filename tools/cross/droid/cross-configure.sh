568b719c1b8b6b04e442336aae9e40009a751ea5
Bill Currie <bill@taniwha.org>
2012-11-29 10:28:46 +0900

    Make a bunch of improvements to the droid cross scripts.
    Native versions of qfcc and pak are now built automatically, and the
    android toolchain now defaults to a more sensible place. Also, the separate
    pkg-config replacement is no longer necessary.

--- tools/cross/droid/cross-configure.sh
+++ tools/cross/droid/cross-configure.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -x
-export ANDROID_NDK_ROOT=/home/bill/Downloads/android-ndk-r7
+set -e
+mkdir -p native android-ndk-r7
+cd native
+../../configure \
+	--disable-shared \
+	--without-clients \
+	--without-servers \
+	--with-tools=qfcc,pak
+cd ../android-ndk-r7
+export ANDROID_NDK_ROOT=/opt/android-ndk-r7
 export ANDROID_SYSROOT=$ANDROID_NDK_ROOT/android-14-toolchain/sysroot
+export PKG_CONFIG_LIBDIR=$ANDROID_SYSROOT
+export PKG_CONFIG_PATH=$ANDROID_SYSROOT/usr/local/lib/pkgconfig
 export PATH=$ANDROID_NDK_ROOT/android-14-toolchain/bin:$PATH
-../configure \
+../../configure \
 	--build=x86_64-unknown-linux-gnu \
 	--with-sysroot=$ANDROID_SYSROOT \
 	--host=arm-linux-androideabi \
--- tools/cross/droid/cross-make.sh
+++ tools/cross/droid/cross-make.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -x
-export ANDROID_NDK_ROOT=/home/bill/Downloads/android-ndk-r7
+set -e
+if test -d native; then
+	cd native
+	make $*
+	cd ../android-ndk-r7
+	ln -fs ../native/tools/qfcc/source/qfcc .
+	ln -fs ../native/tools/pak/pak .
+fi
+export ANDROID_NDK_ROOT=/opt/android-ndk-r7
 export ANDROID_SYSROOT=$ANDROID_NDK_ROOT/android-14-toolchain/sysroot
 export PATH=$ANDROID_NDK_ROOT/android-14-toolchain/bin:$PATH
 
-make PAK=pak QFCC=qfcc $*
+make PAK='$(top_builddir)/pak' QFCC='$(top_builddir)/qfcc' $*
 
