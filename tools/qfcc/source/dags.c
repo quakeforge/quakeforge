7b1850917ddbf1b8a4502df55339a2154bf00712
Bill Currie <bill@taniwha.org>
2012-12-20 21:01:46 +0900

    Force RETURN's operand to be live.
    This seems to be the final fix to get return-ivar.r working.

--- tools/qfcc/source/dags.c
+++ tools/qfcc/source/dags.c
@@ -54,6 +54,7 @@
 #include "strpool.h"
 #include "symtab.h"
 #include "type.h"
+#include "value.h"
 
 static daglabel_t *free_labels;
 static dagnode_t *free_nodes;
@@ -372,6 +373,16 @@ dagnode_set_edges (dag_t *dag, dagnode_t *n)
 			first_param = 2;
 		} else if (!strncmp (n->label->opcode, "<CALL", 5)) {
 			num_params = n->label->opcode + 5;
+		} else if (!strcmp (n->label->opcode, "<RETURN>")) {
+			daglabel_t *label = n->children[0]->label;
+			if (!label->op) {
+				set_iter_t *lab_i;
+				for (lab_i = set_first (n->children[0]->identifiers); lab_i;
+					 lab_i = set_next (lab_i)) {
+					label = dag->labels[lab_i->value];
+				}
+			}
+			label->live = 1;
 		}
 		if (num_params && isdigit (*num_params)) {
 			for (i = first_param; i < *num_params - '0'; i++) {
---
+++ tools/qfcc/test/return-ivar.r
@@ -0,0 +1,26 @@
+typedef struct Point {
+	int x;
+	int y;
+} Point;
+
+@interface Object	//just so the runtime doesn't complain
+{
+	Class isa;
+	Point origin;
+}
+-(Point) origin;
+@end
+@implementation Object
+-(Point) origin
+{
+	Point p;
+	p = origin;
+	return p;
+}
+@end
+void __obj_exec_class (struct obj_module *msg) = #0;
+
+int main()
+{
+	return 0;
+}
