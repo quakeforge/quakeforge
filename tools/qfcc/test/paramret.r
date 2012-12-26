88c765e21020fde06b874a869e84049a9ae7bc1b
Bill Currie <bill@taniwha.org>
2012-12-26 11:49:39 +0900

    Add a test for returning @param into an array.

---
+++ tools/qfcc/test/paramret.r
@@ -0,0 +1,26 @@
+@param
+foo (string str)
+{
+	@param ret;
+	ret = nil;
+	ret.string_val = str;
+	return ret;
+}
+
+string
+bar (string str)
+{
+	@param params[8];
+	@va_list va_list = { 0, params };
+	int j;
+
+	for (j = 0; j < 1; j++)
+		va_list.list[j] = foo (str);
+	return va_list.list[0].string_val;
+}
+
+int
+main ()
+{
+	return bar ("snafu") != "snafu";
+}
