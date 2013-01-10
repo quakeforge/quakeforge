9afeae0c47442fecb6d71292875816412d21a003
Bill Currie <bill@taniwha.org>
2013-01-10 21:13:19 +0900

    Fix the mis-specified PKG_CONFIG_LIBDIR for mips.
    D'oh, forgot usr :P

--- tools/cross/mipsel-linux/cross-make.sh
+++ tools/cross/mipsel-linux/cross-make.sh
@@ -9,7 +9,7 @@ if test -d native; then
 fi
 export MIPSEL=/opt/gcw0-toolchain
 export MIPSEL_SYSROOT=$MIPSEL/usr/mipsel-gcw0-linux-uclibc/sysroot
-export PKG_CONFIG_LIBDIR=$MIPSEL_SYSROOT/lib/pkgconfig
+export PKG_CONFIG_LIBDIR=$MIPSEL_SYSROOT/usr/lib/pkgconfig
 export PKG_CONFIG_PATH=$MIPSEL_SYSROOT/usr/local/lib/pkgconfig
 export PATH=$MIPSEL/usr/bin:$PATH
 
