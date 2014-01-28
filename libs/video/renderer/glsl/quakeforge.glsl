50bb40be29b9eebce55b794ecac5513c0d80164a
Bill Currie <bill@taniwha.org>
2014-01-28 12:31:27 +0900

    Use the new shader system for sprites.

--- libs/video/renderer/glsl/quakeforge.glsl
+++ libs/video/renderer/glsl/quakeforge.glsl
@@ -365,3 +365,72 @@ main (void)
 {
 	gl_FragColor = fogBlend (texture2D (texture, st) * color);
 }
+
+-- Vertex.sprite
+
+uniform mat4 mvp_mat;
+attribute float vblend;
+attribute vec4 vcolora, vcolorb;
+attribute vec4 uvab;	///< ua va ub vb
+attribute vec3 vertexa, vertexb;
+
+varying float blend;
+varying vec4 colora, colorb;
+varying vec2 sta, stb;
+
+void
+main (void)
+{
+	gl_Position = mvp_mat * vec4 (mix (vertexa, vertexb, vblend), 1.0);
+	blend = vblend;
+	colora = vcolora;
+	colorb = vcolorb;
+	sta = uvab.xy;
+	stb = uvab.zw;
+}
+
+-- Fragment.sprite
+
+uniform sampler2D spritea;
+uniform sampler2D spriteb;
+uniform sampler2D palette;
+uniform vec4 fog;
+
+varying float blend;
+varying vec4 colora, colorb;
+varying vec2 sta, stb;
+
+float
+sqr (float x)
+{
+	return x * x;
+}
+
+vec4
+fogBlend (vec4 color)
+{
+	float       f;
+	vec4        fog_color = vec4 (fog.rgb, 1.0);
+
+	f = exp (-sqr (fog.a * gl_FragCoord.z / gl_FragCoord.w));
+	return vec4 (mix (fog_color.rgb, color.rgb, f), color.a);
+}
+
+void
+main (void)
+{
+	float       pixa, pixb;
+	vec4        cola, colb;
+	vec4        col;
+
+	pixa = texture2D (spritea, sta).r;
+	pixb = texture2D (spriteb, stb).r;
+	if (pixa == 1.0 && pixb == 1.0)
+		discard;
+	cola = texture2D (palette, vec2 (pixa, 0.0)) * colora;
+	colb = texture2D (palette, vec2 (pixb, 0.0)) * colorb;
+	col = mix (cola, colb, blend);
+	if (col.a == 0.0)
+		discard;
+	gl_FragColor = fogBlend (col);
+}
