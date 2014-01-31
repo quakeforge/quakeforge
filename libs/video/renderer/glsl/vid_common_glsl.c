77fa66b2e83f192fed1ff1f4abd5127b3a7ff15e
Bill Currie <bill@taniwha.org>
2014-01-31 12:00:19 +0900

    Properly separate the two glsl files in C.

--- libs/video/renderer/glsl/vid_common_glsl.c
+++ libs/video/renderer/glsl/vid_common_glsl.c
@@ -58,7 +58,7 @@
 
 static const char quakeforge_effect[] =
 #include "quakeforge.slc"
-"--"	// ensure the last block of the previous file doesn't merge with
+"--\n"	// ensure the last block of the previous file doesn't merge with
 		// the first block of the next file
 // Include Stefan Gustavson's noise functions in the QuakeForge shader
 // effect "file".
