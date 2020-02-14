7e7e0526dd3c63ee23ed89fd2f055799169e76d6
Bill Currie <bill@taniwha.org>
2020-02-14 20:42:36 +0900

    Fix a double constant issue
    There will be more of these :/

--- ruamoko/game/Axe.r
+++ ruamoko/game/Axe.r
@@ -32,7 +32,7 @@
 	source = s.origin + '0 0 16';
 
 	traceline (source, source + v_forward * 64, NO, s);
-	if (trace_fraction == 1.0)
+	if (trace_fraction == 1.0f)
 		return;
 
 	org = trace_endpos - v_forward * 4;
