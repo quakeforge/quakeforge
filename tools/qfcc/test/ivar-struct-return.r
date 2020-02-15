9c996df7b4689228b6177521dcea4180deb2b925
Bill Currie <bill@taniwha.org>
2020-02-15 19:42:32 +0900

    Add a test case for the uninit temp in HUD.r

---
+++ tools/qfcc/test/ivar-struct-return.r
@@ -0,0 +1,26 @@
+#pragma bug die
+
+struct Point {
+	int x;
+	int y;
+};
+typedef struct Point Point;
+
+@interface Object
+{
+	Point origin;
+}
+-(Point) origin;
+@end
+
+@implementation Object
+-(Point) origin
+{
+	return origin;
+}
+@end
+void __obj_exec_class (struct obj_module *msg) = #0;
+int main()
+{
+	return 0;	// to survive and prevail
+}
