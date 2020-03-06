6bf2fd546d945e5171d92d1176286915e77ccebe
Bill Currie <bill@taniwha.org>
2020-03-06 11:27:31 +0900

    [qwaq] Extend Array makeObjectsPerform methods
    Added conditional message sending and reversed traversal of the array.

--- ruamoko/qwaq/qwaq-group.h
+++ ruamoko/qwaq/qwaq-group.h
@@ -1,9 +1,31 @@
 #ifndef __qwaq_group_h
 #define __qwaq_group_h
 
+#include <Array.h>
 #include "qwaq-view.h"
 
-@class Array;
+typedef BOOL condition_func (id object, void *data);
+typedef BOOL condition_func2 (id object, void *anObject, void *data);
+
+@interface Array (Group)
+- (void) makeObjectsPerformSelector: (SEL)selector
+								 if: (condition_func)condition
+							   with: (void *)data;
+- (void) makeObjectsPerformSelector: (SEL)selector
+						 withObject: (void *)anObject
+								 if: (condition_func2)condition
+							   with: (void *)data;
+- (void) makeReversedObjectsPerformSelector: (SEL)selector;
+- (void) makeReversedObjectsPerformSelector: (SEL)selector
+								 withObject: (void *)anObject;
+- (void) makeReversedObjectsPerformSelector: (SEL)selector
+										 if: (condition_func)condition
+									   with: (void *)data;
+- (void) makeReversedObjectsPerformSelector: (SEL)selector
+								 withObject: (void *)anObject
+										 if: (condition_func2)condition
+									   with: (void *)data;
+@end
 
 @interface Group : View
 {
