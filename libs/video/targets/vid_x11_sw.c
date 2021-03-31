8e363d424f0461c8ac76c8f51bd2eb33341e351e
Bill Currie <bill@taniwha.org>
2021-03-31 15:11:31 +0900

    [vid] Remove redundant call to VID_InitBuffers

--- libs/video/targets/vid_x11_sw.c
+++ libs/video/targets/vid_x11_sw.c
@@ -519,8 +519,6 @@ x11_create_context (sw_ctx_t *ctx)
 	}
 
 	viddef.vid_internal->init_buffers = x11_init_buffers;
-	VID_InitBuffers ();
-
 //  XSynchronize (x_disp, False);
 //	X11_AddEvent (x_shmeventtype, event_shm);
 }
