18247a8c8e23676e7048409fd5075cf607c943d4
Bill Currie <bill@taniwha.org>
2021-04-02 08:55:16 +0900

    [tools] Fix up 32-bit cross-compile scripts

--- tools/cross/mingw/cross-configure.sh
+++ tools/cross/mingw/cross-configure.sh
@@ -1,19 +1,20 @@
 #!/bin/sh -x
 set -e
-mkdir -p native i686-pc-mingw32
+mkdir -p native i686-w64-mingw32.static
 cd native
 ../../configure \
+	--enable-silent-rules \
 	--disable-shared \
 	--without-clients \
 	--without-servers \
-	--with-tools=qfcc,pak
-cd ../i686-pc-mingw32
+	--with-tools=qfcc,pak,qwaq
+cd ../i686-w64-mingw32.static
 export MINGW=/opt/mxe
-export MINGW_USR=$MINGW/usr/i686-pc-mingw32
+export MINGW_USR=$MINGW/usr/i686-w64-mingw32.static
 export PKG_CONFIG_LIBDIR=$MINGW_USR/lib/pkgconfig
 export PKG_CONFIG_PATH=$MINGW_USR/local/lib/pkgconfig
 export PATH=$MINGW/usr/bin:$PATH
 ../../configure \
-	--host=i686-pc-mingw32 \
+	--host=i686-w64-mingw32.static \
 	--disable-shared \
 	$*
