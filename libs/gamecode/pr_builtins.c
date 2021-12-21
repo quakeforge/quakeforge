0c41c11f1d1ded3658a48f2ded559ce496c20938
Bill Currie <bill@taniwha.org>
2021-12-22 06:55:30 +0900

    [gamecode] Use a new developer flag for builtin debug
    rua_resolve works for now

--- libs/gamecode/pr_builtins.c
+++ libs/gamecode/pr_builtins.c
@@ -220,7 +220,7 @@ PR_RelocateBuiltins (progs_t *pr)
 			ind = pr->bi_map (pr, ind);
 		bi = PR_FindBuiltinNum (pr, ind);
 		if (!bi || !(proc = bi->proc)) {
-			Sys_MaskPrintf (SYS_dev,
+			Sys_MaskPrintf (SYS_rua_resolve,
 							"WARNING: Bad builtin call number: %s = #%d\n",
 							bi_name, -desc->first_statement);
 			proc = bi_no_function;
