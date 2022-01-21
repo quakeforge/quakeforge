quakeforge/quakeforge

commit 76b3bedb72a0a1b92e658c733bf6ff079d18ce10

[qfcc] Revert "Kill dag leaf nodes on assignment."
This reverts commit 2fcda44ab081ec4425d6f54923109bfd5131bd57.

Killing the node is not the correcgt answer as it blocks many
optimization opportunities. The correct answer is adding edges to
describe the temporal dependencies. Of course, this breaks the swap.r
test.

tools/qfcc/source/dags.c
@@ -640,11 +640,6 @@ dagnode_attach_label (dagnode_t *n, daglabel_t *l)
 						"attempt to attach non-identifer label to dagnode "
 						"identifiers");
 	if (l->dagnode) {
-		// if the node is a leaf, then kill its value so no attempt is made
-		// to reuse it.
-		if (l->dagnode->type == st_none) {
-			l->dagnode->killed = n;
-		}
 		dagnode_t  *node = l->dagnode;
 		set_union (n->edges, node->parents);
 		set_remove (n->edges, n->number);

commit 64da1b603a3ccc0cf53657f84b2c131f1bba52a0

[qfcc] Add failing test for return of postop
Commit 76b3bedb72a0a1b92e658c733bf6ff079d18ce10 broke more than just the
swap test, but at least I know I need to get an edge in the dag.
Currently, the following code is generated: return and add are reversed.

    ../tools/qfcc/test/return-postop.r:8:   return counter++;
    0001 store.i counter, .tmp0
    0002 return .tmp0
    0003 add.i .tmp0, (1), counter

However, I don't want to deal with it right now, so it's marked XFAIL.

tools/qfcc/test/Makemodule.am
@@ -41,6 +41,7 @@ test_progs_dat=\
 	tools/qfcc/test/ptrstructcast.dat \
 	tools/qfcc/test/quaternion.dat \
 	tools/qfcc/test/return-ivar.dat \
+	tools/qfcc/test/return-postop.dat \
 	tools/qfcc/test/sendv.dat \
 	tools/qfcc/test/state.dat \
 	tools/qfcc/test/static-init.dat \
@@ -62,7 +63,8 @@ test_progs_dat=\
 	tools/qfcc/test/while.dat \
 	tools/qfcc/test/zerolinker.dat
 
-fail_progs_dat=
+fail_progs_dat=\
+	tools/qfcc/test/return-postop.dat
 
 test_build_errors=\
 	tools/qfcc/test/classarray.r \
@@ -482,6 +484,16 @@ tools/qfcc/test/return-ivar.run: $(qfcc_test_run_deps)
 	@$(top_srcdir)/tools/qfcc/test/build-run $@
 include $(return_ivar_dep) # am--include-marker
 r_depfiles_remade += $(return_ivar_dep)
+
+tools_qfcc_test_return_postop_dat_SOURCES=tools/qfcc/test/return-postop.r
+return_postop_obj=$(tools_qfcc_test_return_postop_dat_SOURCES:.r=.o)
+return_postop_dep=$(call qcautodep,$(tools_qfcc_test_return_postop_dat_SOURCES))
+tools/qfcc/test/return-postop.dat$(EXEEXT): $(return_postop_obj) $(QFCC_DEP)
+	$(V_QFCCLD)$(QLINK) -o $@ $(return_postop_obj)
+tools/qfcc/test/return-postop.run: $(qfcc_test_run_deps)
+	@$(top_srcdir)/tools/qfcc/test/build-run $@
+include $(return_postop_dep) # am--include-marker
+r_depfiles_remade += $(return_postop_dep)
 
 tools_qfcc_test_sendv_dat_SOURCES=tools/qfcc/test/sendv.r
 sendv_obj=$(tools_qfcc_test_sendv_dat_SOURCES:.r=.o)

tools/qfcc/test/return-postop.r
@@ -1,0 +1,22 @@
+#include "test-harness.h"
+
+int counter;
+
+int
+function (void)
+{
+	return counter++;
+}
+
+int
+main (void)
+{
+	int ret = 0;
+	counter = 0;
+	function ();
+	if (counter != 1) {
+		printf ("counter != 1: %d\n", counter);
+		ret = 1;
+	}
+	return ret;
+}