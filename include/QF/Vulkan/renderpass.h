6a7c2cb2a1a59e5cf90bf800fb0223ff8cf629c8
Bill Currie <bill@taniwha.org>
2022-05-30 11:06:13 +0900

    [vulkan] Clean out the old render pass creation code
    QFV_CreateRenderPass is no longer used, and QFV_CreateFramebuffer hasn't
    been used for a long time. The C file is still there for now but is
    basically empty.

--- include/QF/Vulkan/renderpass.h
+++ include/QF/Vulkan/renderpass.h
@@ -4,49 +4,11 @@
 #include "QF/darray.h"
 #include "QF/simd/types.h"
 
-typedef struct qfv_attachmentdescription_s
-	DARRAY_TYPE (VkAttachmentDescription) qfv_attachmentdescription_t;
-
-#define QFV_AllocAttachmentDescription(num, allocator) \
-	DARRAY_ALLOCFIXED (qfv_attachmentdescription_t, num, allocator)
-
-typedef struct qfv_attachmentreference_s
-	DARRAY_TYPE (VkAttachmentReference) qfv_attachmentreference_t;
-
-#define QFV_AllocAttachmentReference(num, allocator) \
-	DARRAY_ALLOCFIXED (qfv_attachmentreference_t, num, allocator)
-
-typedef struct qfv_subpassparametersset_s
-	DARRAY_TYPE (VkSubpassDescription) qfv_subpassparametersset_t;
-
-#define QFV_AllocSubpassParametersSet(num, allocator) \
-	DARRAY_ALLOCFIXED (qfv_subpassparametersset_t, num, allocator)
-
-typedef struct qfv_subpassdependency_s
-	DARRAY_TYPE (VkSubpassDependency) qfv_subpassdependency_t;
-
-#define QFV_AllocSubpassDependencies(num, allocator) \
-	DARRAY_ALLOCFIXED (qfv_subpassdependency_t, num, allocator)
-
 typedef struct qfv_framebufferset_s
 	DARRAY_TYPE (VkFramebuffer) qfv_framebufferset_t;
 
 #define QFV_AllocFrameBuffers(num, allocator) \
 	DARRAY_ALLOCFIXED (qfv_framebufferset_t, num, allocator)
-
-struct qfv_device_s;
-struct qfv_imageviewset_s;
-VkRenderPass
-QFV_CreateRenderPass (struct qfv_device_s *device,
-					  qfv_attachmentdescription_t *attachments,
-					  qfv_subpassparametersset_t *subpasses,
-					  qfv_subpassdependency_t *dependencies);
-
-VkFramebuffer
-QFV_CreateFramebuffer (struct qfv_device_s *device,
-					   VkRenderPass renderPass,
-					   struct qfv_imageviewset_s *attachments,
-					   VkExtent2D, uint32_t layers);
 
 typedef struct qfv_subpass_s {
 	vec4f_t     color;
@@ -57,7 +19,7 @@ typedef struct qfv_renderframe_s {
 	struct vulkan_ctx_s *vulkan_ctx;
 	struct qfv_renderpass_s *renderpass;
 	VkSubpassContents subpassContents;
-	int subpassCount;
+	int         subpassCount;
 	qfv_subpass_t *subpassInfo;
 	struct qfv_cmdbufferset_s *subpassCmdSets;
 } qfv_renderframe_t;
