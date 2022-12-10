c316b6b5d193bf27a2abce9e72386c2276846fd3
Bill Currie <bill@taniwha.org>
2022-12-10 18:57:51 +0900

    [ruamoko] Wrap more text and view functions
    rua_gui.c is still a horrible mess of hacks, but it does the job for
    testing the text code.

--- libs/ruamoko/rua_gui.c
+++ libs/ruamoko/rua_gui.c
@@ -115,7 +115,7 @@ alloc_passage (gui_resources_t *res, passage_t *passage)
 	return passage_index (res, psg);
 }
 
-static rua_passage_t *
+static rua_passage_t * __attribute__((pure))
 _get_passage (gui_resources_t *res, int handle, const char *func)
 {
 	rua_passage_t *psg = passage_get (res, handle);
@@ -171,7 +171,7 @@ alloc_font (gui_resources_t *res, font_t *font)
 	return font_index (res, psg);
 }
 
-static rua_font_t *
+static rua_font_t * __attribute__((pure))
 _get_font (gui_resources_t *res, int handle, const char *func)
 {
 	rua_font_t *psg = font_get (res, handle);
@@ -247,6 +247,26 @@ bi (Passage_Delete)
 	passage_free (res, psg);
 }
 
+bi (Passage_ChildCount)
+{
+	gui_resources_t *res = _res;
+	rua_passage_t *psg = get_passage (res, P_INT (pr, 0));
+
+	uint32_t    par = P_UINT (pr, 1);
+	R_UINT (pr) = psg->passage->hierarchy->childCount[par];
+}
+
+bi (Passage_GetChild)
+{
+	gui_resources_t *res = _res;
+	rua_passage_t *psg = get_passage (res, P_INT (pr, 0));
+
+	hierarchy_t *h = psg->passage->hierarchy;
+	uint32_t    par = P_UINT (pr, 1);
+	uint32_t    index = P_UINT (pr, 2);
+	R_UINT (pr) = h->ent[h->childIndex[par] + index];
+}
+
 bi (Text_View)
 {
 	gui_resources_t *res = _res;
@@ -254,6 +274,124 @@ bi (Text_View)
 	rua_passage_t *psg = get_passage (res, P_INT (pr, 1));
 	view_t      view = Text_View (font->font, psg->passage);
 	R_INT (pr) = view.id;//FIXME
+}
+
+bi (Text_SetScript)
+{
+	uint32_t    textent = P_UINT (pr, 0);
+	const char *lang = P_GSTRING (pr, 1);
+	int         script = P_INT (pr, 1);
+	int         dir = P_INT (pr, 1);
+	Text_SetScript (textent, lang, script, dir);
+}
+
+static void
+draw_glyphs (view_pos_t *abs, glyphset_t *glyphset, glyphref_t *gref)
+{
+	uint32_t    count = gref->count;
+	glyphobj_t *glyph = glyphset->glyphs + gref->start;
+
+	while (count-- > 0) {
+		glyphobj_t *g = glyph++;
+		r_funcs->Draw_Glyph (abs->x + g->x, abs->y + g->y,
+							 g->fontid, g->glyphid, 254);
+	}
+}
+
+static void
+draw_box (view_pos_t *abs, view_pos_t *len, uint32_t ind, int c)
+{
+	int x = abs[ind].x;
+	int y = abs[ind].y;
+	int w = len[ind].x;
+	int h = len[ind].y;
+	r_funcs->Draw_Line (x, y, x + w, y, c);
+	r_funcs->Draw_Line (x, y + h, x + w, y + h, c);
+	r_funcs->Draw_Line (x, y, x, y + h, c);
+	r_funcs->Draw_Line (x + w, y, x + w, y + h, c);
+}
+
+bi (Text_Draw)
+{
+	ecs_pool_t *pool = &text_reg->comp_pools[text_passage_glyphs];
+	uint32_t    count = pool->count;
+	uint32_t   *ent = pool->dense;
+	glyphset_t *glyphset = pool->data;
+
+
+	while (count-- > 0) {
+		view_t      psg_view = { .id = *ent++, .reg = text_reg,
+								.comp = text_reg->href_comp};
+		// first child is always a paragraph view, and all vies after the
+		// first paragraph's first child are all text views
+		view_t      para_view = View_GetChild (psg_view, 0);
+		view_t      text_view = View_GetChild (para_view, 0);
+		hierref_t  *href = View_GetRef (text_view);
+		hierarchy_t *h = href->hierarchy;
+		view_pos_t *abs = h->components[view_abs];
+		view_pos_t *len = h->components[view_len];
+
+		for (uint32_t i = href->index; i < h->num_objects; i++) {
+			glyphref_t *gref = Ent_GetComponent (h->ent[i], text_glyphs,
+												 text_reg);
+			draw_glyphs (&abs[i], glyphset, gref);
+
+			if (0) draw_box (abs, len, i, 253);
+		}
+		if (0) {
+			for (uint32_t i = 1; i < href->index; i++) {
+				draw_box (abs, len, i, 251);
+			}
+		}
+	}
+}
+
+bi (View_ChildCount)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	R_UINT (pr) = View_ChildCount (view);
+}
+
+bi (View_GetChild)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	uint32_t    index = P_UINT (pr, 1);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	R_UINT (pr) = View_GetChild (view, index).id;
+}
+
+bi (View_SetPos)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	int         x = P_INT (pr, 1);
+	int         y = P_INT (pr, 2);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	View_SetPos (view, x, y);
+}
+
+bi (View_SetLen)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	int         x = P_INT (pr, 1);
+	int         y = P_INT (pr, 2);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	View_SetLen (view, x, y);
+}
+
+bi (View_GetLen)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	view_pos_t  l = View_GetLen (view);
+	R_var (pr, ivec2) = (pr_ivec2_t) { l.x, l.y };
+}
+
+bi (View_UpdateHierarchy)
+{
+	uint32_t    viewid = P_UINT (pr, 0);
+	view_t      view = { .id = viewid, .reg = text_reg };
+	View_UpdateHierarchy (view);
 }
 
 #undef bi
@@ -265,8 +403,20 @@ static builtin_t builtins[] = {
 	bi(Passage_New,         0),
 	bi(Passage_ParseText,   2, p(ptr), p(string)),
 	bi(Passage_Delete,      1, p(ptr)),
+	bi(Passage_ChildCount,  2, p(ptr), p(uint)),
+	bi(Passage_GetChild,    3, p(ptr), p(uint), p(uint)),
 
-	bi(Text_View,           2, p(ptr), p(string)),
+	bi(Text_View,           2, p(ptr), p(int)),
+	bi(Text_SetScript,      4, p(uint), p(string), p(int), p (int)),
+
+	bi(View_ChildCount,     1, p(uint)),
+	bi(View_GetChild,       2, p(uint), p(uint)),
+	bi(View_SetPos,         3, p(uint), p(int), p(int)),
+	bi(View_SetLen,         3, p(uint), p(int), p(int)),
+	bi(View_GetLen,         1, p(uint)),
+	bi(View_UpdateHierarchy,1, p(uint)),
+
+	bi(Text_Draw,           0),
 
 	{0}
 };
