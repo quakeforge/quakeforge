717cbaff87b399d15e20dba63c6d051524ffae28
Bill Currie <bill@taniwha.org>
2023-05-14 12:32:35 +0900

    [gamecode] Support offset for lea on globals
    Allows using `array + offset` addressing. Both constant and variable
    offsets are supported.

--- libs/gamecode/test/test-lea.c
+++ libs/gamecode/test/test-lea.c
@@ -13,7 +13,7 @@ static pr_int_t lea_globals_expect[] = {
 	// pointers
 	24, 26, 28, 29,
 	32, -4, -2, 0,
-	1, 4, 0xdeadbeef, 0xfeedf00d,
+	1, 4, 6, 32,
 
 	7, 34, 26, 88,
 };
@@ -23,6 +23,8 @@ static dstatement_t lea_statements[] = {
 	{OP(0, 0, 0, OP_LEA_C), 2, 6, 13},
 	{OP(0, 0, 0, OP_LEA_D), 2, 6, 14},
 	{OP(0, 0, 0, OP_LEA_B), 4, 2, 15},
+	{OP(0, 0, 0, OP_LEA_E), 4, 2, 10},
+	{OP(0, 0, 0, OP_LEA_F), 4, 2, 11},
 };
 
 test_t tests[] = {
