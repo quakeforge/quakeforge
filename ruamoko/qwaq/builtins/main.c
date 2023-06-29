dc61d153402ef5509444b215c39a408cedda12e3
Bill Currie <bill@taniwha.org>
2023-06-29 23:33:54 +0900

    [qfcc] Add failing test for array-typedef interaction
    Two variables declared as arrays (same size) of different typedefs to
    the same base type have their type encodings both pointing to the same
    short alias.
    
    From vkgen:
    
        51d3  ty_array  [4={int32_t>i}]      207f  0 4
        51d9  ty_array  [4=i]                1035  0 4
        51df  ty_alias  {>[4=i]}              16  51d9  51e6
        51e6  ty_array  [4={uint32_t>i}]     2063  0 4
        51ec  ty_union  {tag VkClearColorValue-} tag VkClearColorValue
    	    4ca0     0 float32
    	    51df     0 int32
    	    51df     0 uint32
    
    uint32 should use 51e6 and int32 should use 513d,

--- ruamoko/qwaq/builtins/main.c
+++ ruamoko/qwaq/builtins/main.c
@@ -149,7 +149,7 @@ init_qf (void)
 
 	//Cvar_Set (developer, "1");
 
-	Memory_Init (Sys_Alloc (8 * 1024 * 1024), 8 * 1024 * 1024);
+	Memory_Init (Sys_Alloc (32 * 1024 * 1024), 32 * 1024 * 1024);
 	PR_Init_Cvars ();
 }
 
@@ -242,7 +242,7 @@ load_progs (progs_t *pr, const char *name)
 	}
 	pr->progs_name = name;
 	pr->max_edicts = 1;
-	pr->zone_size = 2*1024*1024;
+	pr->zone_size = 8*1024*1024;
 	pr->stack_size = 64*1024;
 	PR_LoadProgsFile (pr, file, size);
 	Qclose (file);
