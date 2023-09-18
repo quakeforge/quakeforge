7646f6b51e1e542b5070654c8a2f7d22467167d6
Bill Currie <bill@taniwha.org>
2023-09-18 19:30:06 +0900

    [qfcc] Add failing test case for motor-point sandwich
    This tests a subtle bug where part of the product is dropped on the
    floor due to an issue with aliasing through expand expressions.

---
+++ tools/qfcc/test/motor-point.r
@@ -0,0 +1,30 @@
+#include "test-harness.h"
+
+typedef @algebra(float(3,0,1)) PGA;
+typedef PGA.group_mask(0xa) bivector_t;
+typedef PGA.group_mask(0x1e) motor_t;
+typedef PGA.tvec point_t;
+
+point_t
+apply_motor (motor_t m, point_t p)
+{
+	return (m * p * ~m).tvec;
+}
+
+int
+main (void)
+{
+	motor_t m = {
+		.scalar = 0.707106769,
+		.bvect = (PGA.bvect) '0 0 0.707106769'f,
+		.bvecp = (PGA.bvecp) '0 -4.2426405 0'f,
+		.qvec = (PGA.qvec) 0,
+	};
+	point_t p = (point_t)'10 4 -1.5 1'f;
+	point_t n = apply_motor (m, p);
+	printf ("n: %.9q\n", n);
+	if ((vec4)n != '10 -4 -1.49999988 0.99999994'f) {
+		return 1;
+	}
+	return 0;
+}
