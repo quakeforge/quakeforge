3a31fa111db0076b7efe8491d00ceff91c4e54fc
Bill Currie <bill@taniwha.org>
2023-12-14 09:56:53 +0900

    [vulkan] Create render passes for up to 32 views
    Actually, only 29 are used because nvidia's drivers segfault when there
    are more than 29 views (regardless of the exact bit pattern in the view
    mask). This will allow rendering shadow maps in large batches, which
    should make for better GPU utilization.

--- libs/video/renderer/vulkan/vulkan_lighting.c
+++ libs/video/renderer/vulkan/vulkan_lighting.c
@@ -81,6 +81,9 @@
 #define shadow_quanta 128
 #define lnearclip 4
 #define num_cascade 4
+#define max_views 29	// FIXME should be 32 (or really, maxMultiviewViewCount,
+						// but there are other problems there), but nvidia's
+						// drivers segfault for > 29
 
 static vec4f_t  ref_direction = { 1, 0, 0, 0 };
 
