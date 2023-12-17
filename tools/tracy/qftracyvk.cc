dd4f51049dc68ba6a8fc5154078d3f23aeb74297
Bill Currie <bill@taniwha.org>
2023-12-17 18:56:36 +0900

    [build] Fix up unwanted dependency on tracy source
    The tracy source files should not be required for make dist to work, so
    this works around it by using a little indirection.

---
+++ tools/tracy/qftracyvk.cc
@@ -0,0 +1,5 @@
+// this works around a limitation with automake and source files that are
+// not part of the distribution
+#ifdef HAVE_TRACY
+#include "tracy/public/client/TracyVkProfiler.cpp"
+#endif
