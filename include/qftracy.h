bfa7c1722aa832dd7a28d71d01cdb6333c0c5e15
Bill Currie <bill@taniwha.org>
2024-01-03 18:22:13 +0900

    [build] Implement tracy memory zones
    This proved to be quite the challenge, and is probably rather fragile,
    but it does seem to work, and might help with tracking down memory
    leaks.

--- include/qftracy.h
+++ include/qftracy.h
@@ -41,6 +41,33 @@ static inline void __qfZoneEnd (TracyCZoneCtx **ctxptr)
 
 #define qfMessageL(msg) TracyCMessageL(msg)
 
+#ifndef __cplusplus
+#define aligned_alloc(a, s) qf_aligned_alloc(a, s)
+#define malloc(s) qf_malloc(s)
+#define calloc(c,s) qf_calloc(c,s)
+#define realloc(p,s) qf_realloc(p,s)
+#define free(p) qf_free(p)
+#define strdup(s) qf_strdup(s)
+#include <stdlib.h>
+#undef aligned_alloc
+#undef malloc
+#undef calloc
+#undef realloc
+#undef free
+#undef strdup
+#define getcwd(b,s) qf_getcwd(b,s)
+#include <unistd.h>
+#undef getcwd
+
+void *aligned_alloc (size_t a, size_t s) asm("__qf_aligned_alloc");
+void *malloc (size_t s) asm("__qf_malloc");
+void *calloc (size_t c, size_t s) asm("__qf_calloc");
+void *realloc (void *p, size_t s) asm("__qf_realloc");
+void free (void *p) asm("__qf_free");
+char *strdup (const char *s) asm("__qf_strdup");
+char *getcwd (char *b, size_t s) asm("__qf_getcwd");
+#endif
+
 #else
 
 #define qfFrameMark
