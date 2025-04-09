37eecaf5593af50176fd526145a323d9ce882366
Bill Currie <bill@taniwha.org>
2025-04-09 09:11:39 +0900

    [ecs] Move entity.h into ecs.h
    I don't know why I thought it was useful to have them separate. It has
    proven to be a bit inconvenient at times.

--- include/QF/ecs.h
+++ include/QF/ecs.h
@@ -111,12 +111,13 @@ typedef struct ecs_system_s {
 	uint32_t    base;
 } ecs_system_t;
 
-#include "QF/ecs/entity.h"
-
 #define ECSINLINE GNU89INLINE inline
 
 uint32_t ECS_NewId (ecs_idpool_t *idpool);
 bool ECS_DelId (ecs_idpool_t *idpool, uint32_t id);
+ECSINLINE uint32_t Ent_Index (uint32_t id);
+ECSINLINE uint32_t Ent_Generation (uint32_t id);
+ECSINLINE uint32_t Ent_NextGen (uint32_t id);
 
 ecs_registry_t *ECS_NewRegistry (const char *name);
 void ECS_DelRegistry (ecs_registry_t *registry);
@@ -150,12 +151,40 @@ ECSINLINE ecs_range_t ECS_GetSubpoolRange (ecs_registry_t *registry,
 void ECS_MoveSubpoolLast (ecs_registry_t *registry, uint32_t component,
 						  uint32_t id);
 
+ECSINLINE int ECS_EntValid (uint32_t id, ecs_registry_t *reg);
+ECSINLINE int Ent_HasComponent (uint32_t ent, uint32_t comp,
+								ecs_registry_t *reg);
+ECSINLINE void *Ent_GetComponent (uint32_t ent, uint32_t comp,
+								  ecs_registry_t *reg);
+ECSINLINE void *Ent_SafeGetComponent (uint32_t ent, uint32_t comp,
+									  ecs_registry_t *reg);
+ECSINLINE void *Ent_SetComponent (uint32_t ent, uint32_t comp,
+								  ecs_registry_t *registry, const void *data);
+
 #undef ECSINLINE
 #ifndef IMPLEMENT_ECS_Funcs
 #define ECSINLINE GNU89INLINE inline
 #else
 #define ECSINLINE VISIBLE
 #endif
+
+ECSINLINE uint32_t
+Ent_Index (uint32_t id)
+{
+	return id & ((1 << ENT_IDBITS) - 1);
+}
+
+ECSINLINE uint32_t
+Ent_Generation (uint32_t id)
+{
+	return id & ~((1 << ENT_IDBITS) - 1);
+}
+
+ECSINLINE uint32_t
+Ent_NextGen(uint32_t id)
+{
+	return id + (1 << ENT_IDBITS);
+}
 
 ECSINLINE
 ecs_range_t
@@ -168,6 +197,60 @@ ECS_GetSubpoolRange (ecs_registry_t *registry, uint32_t component, uint32_t id)
 		.end = subpool->ranges[ind],
 	};
 	return range;
+}
+
+ECSINLINE int
+ECS_EntValid (uint32_t id, ecs_registry_t *reg)
+{
+	uint32_t    ind = Ent_Index (id);
+	return ind < reg->entities.num_ids && reg->entities.ids[ind] == id;
+}
+
+ECSINLINE int
+Ent_HasComponent (uint32_t ent, uint32_t comp, ecs_registry_t *reg)
+{
+	ecs_pool_t *pool = &reg->comp_pools[comp];
+	uint32_t    ind = pool->sparse[Ent_Index (ent)];
+	return ind < pool->count && pool->dense[ind] == ent;
+}
+
+ECSINLINE void *
+Ent_GetComponent (uint32_t ent, uint32_t comp, ecs_registry_t *reg)
+{
+	const component_t *component = &reg->components.a[comp];
+	uint32_t    ind = reg->comp_pools[comp].sparse[Ent_Index (ent)];
+	byte       *data = reg->comp_pools[comp].data;
+	return data + ind * component->size;
+}
+
+ECSINLINE void *
+Ent_SafeGetComponent (uint32_t ent, uint32_t comp, ecs_registry_t *reg)
+{
+	if (!ECS_EntValid (ent, reg) || !Ent_HasComponent (ent, comp, reg)) {
+		return 0;
+	}
+	const component_t *component = &reg->components.a[comp];
+	uint32_t    ind = reg->comp_pools[comp].sparse[Ent_Index (ent)];
+	byte       *data = reg->comp_pools[comp].data;
+	return data + ind * component->size;
+}
+
+void *Ent_AddComponent (uint32_t ent, uint32_t comp, ecs_registry_t *registry);
+void Ent_RemoveComponent (uint32_t ent, uint32_t comp,
+						  ecs_registry_t *registry);
+
+ECSINLINE void *
+Ent_SetComponent (uint32_t ent, uint32_t comp, ecs_registry_t *registry,
+				  const void *data)
+{
+	void       *dst = Ent_AddComponent (ent, comp, registry);
+	if (data) {
+		return Component_CopyElements (&registry->components.a[comp],
+									   dst, 0, data, 0, 1);
+	} else {
+		return Component_CreateElements (&registry->components.a[comp],
+										 dst, 0, 1);
+	}
 }
 
 #undef ECSINLINE
