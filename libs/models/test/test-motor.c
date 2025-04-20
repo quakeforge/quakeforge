0f767e9e277dddb6716ca12da4df58deb78fe53e
Bill Currie <bill@taniwha.org>
2025-04-20 19:27:05 +0900

    [model] expand motor tests to include products
    It turns out the motor products were fine, it was just non-uniform
    scaling messing things up, thus the commented out scales in the test
    data.

--- libs/models/test/test-motor.c
+++ libs/models/test/test-motor.c
@@ -26,12 +26,46 @@ compare (float a, float b, int bits)
 	}
 	typedef union {
 		float   f;
-		int32_t i;
+		uint32_t i;
 	} fi_t;
 	fi_t ia = { .f = a };
 	fi_t ib = { .f = b };
-	int32_t mask = ~((1 << bits) - 1);
-	return ((ia.i - ib.i) & mask) == 0;
+	uint32_t mask = ~((1 << bits) - 1);
+	uint32_t diff;
+	if (ia.i > ib.i) {
+		diff = ia.i - ib.i;
+	} else {
+		diff = ib.i - ia.i;
+	}
+	printf ("%08x %08x %08x %08x\n", mask, diff, ia.i, ib.i);
+	return ((diff) & mask) == 0;
+}
+
+static bool
+check_mats (mat4f_t jmat, mat4f_t mmat, int bits)
+{
+	printf ("jmat%*smmat\n", 38, "");
+	auto jstr = dstring_new ();
+	auto mstr = dstring_new ();
+	bool pass = true;
+	for (int i = 0; i < 4; i++) {
+		dsprintf (jstr, "[");
+		dsprintf (mstr, "[");
+		for (int j = 0; j < 4; j++) {
+			const char *sep = j ? "," : "";
+			bool ok = compare (jmat[j][i], mmat[j][i], bits);
+			const char *col = ok ? GRN : RED;
+			pass &= ok;
+			dasprintf (jstr, "%s%s%9.6f%s", sep, col, jmat[j][i], DFL);
+			dasprintf (mstr, "%s%s%9.6f%s", sep, col, mmat[j][i], DFL);
+		}
+		dasprintf (jstr, "]");
+		dasprintf (mstr, "]");
+		printf ("%-40s %s\n", jstr->str, mstr->str);
+	}
+	dstring_delete (jstr);
+	dstring_delete (mstr);
+	return pass;
 }
 
 static bool
@@ -52,27 +86,38 @@ test_motor (qfm_joint_t joint)
 	printf ("m: " VEC4F_FMT " " VEC4F_FMT " " VectorFMT "\n",
 			VEC4_EXP (motor.q), VEC4_EXP (motor.t), VectorExpand (motor.s));
 
-	printf ("jmat%*smmat\n", 38, "");
-	auto jstr = dstring_new ();
-	auto mstr = dstring_new ();
-	bool pass = true;
-	for (int i = 0; i < 4; i++) {
-		dsprintf (jstr, "[");
-		dsprintf (mstr, "[");
-		for (int j = 0; j < 4; j++) {
-			const char *sep = j ? "," : "";
-			bool ok = compare (jmat[j][i], mmat[j][i], 2);
-			const char *col = ok ? GRN : RED;
-			pass &= ok;
-			dasprintf (jstr, "%s%s%9.6f%s", sep, col, jmat[j][i], DFL);
-			dasprintf (mstr, "%s%s%9.6f%s", sep, col, mmat[j][i], DFL);
-		}
-		dasprintf (jstr, "]");
-		dasprintf (mstr, "]");
-		printf ("%-40s %s\n", jstr->str, mstr->str);
+	return check_mats (jmat, mmat, 2);
+}
+
+static bool
+test_chain (const qfm_joint_t *chain, int chain_len)
+{
+	qfm_joint_t joints[chain_len];
+	for (int i = 0; i < chain_len; i++) {
+		// due to how the data was obtained, the incoming chain is reversed
+		// (root is last)
+		joints[i] = chain[chain_len - i - 1];
 	}
-	dstring_delete (jstr);
-	dstring_delete (mstr);
+	qfm_motor_t motors[chain_len];
+	mat4f_t motor_mats[chain_len];
+	mat4f_t joint_mats[chain_len];
+	for (int i = 0; i < chain_len; i++) {
+		joints[i].rotate = normalf (joints[i].rotate);
+		motors[i] = qfm_make_motor (joints[i]);
+		qfm_motor_to_mat (motor_mats[i], motors[i]);
+		set_matrix (joint_mats[i],
+					joints[i].translate, joints[i].rotate, joints[i].scale);
+		if (i > 0) {
+			motors[i] = qfm_motor_mul (motors[i - 1], motors[i]);
+			qfm_motor_to_mat (motor_mats[i], motors[i]);
+			mmulf (joint_mats[i], joint_mats[i - 1], joint_mats[i]);
+		}
+	}
+
+	bool pass = true;
+	for (int i = 0; i < chain_len; i++) {
+		pass &= check_mats (joint_mats[i], motor_mats[i], 12);
+	}
 	return pass;
 }
 
@@ -107,6 +152,33 @@ static qfm_joint_t joints[] = {
 	},
 };
 
+static qfm_joint_t chain_joints[] = {
+	{ .translate = {-7.04858394e-12, 0.129213497, -1.48989443e-08},
+	  .rotate = {-0.436430037, 0.175517023, -0.0877642334, -0.878077865},
+	  //.scale = {0.999649048, 0.971740901, 1.03048599}
+	  .scale = {1, 1, 1}
+	},
+	{ .translate = {2.23605525e-08, 0.129213542, -4.470985e-08},
+	  .rotate = {-7.60717285e-08, 0.173551738, -9.35314119e-08, -0.984822154},
+	  .scale = {1, 1, 1}
+	},
+	{ .translate = {0.00694327988, 0.172899857, -0.0175056811},
+	  .rotate = {0.0997654796, -0.709096134, 0.4472965, -0.535878003},
+	  //.scale = {1.01577735, 0.969177067, 1.01577735}
+	  .scale = {1, 1, 1}
+	},
+	{ .translate = {0.0158015266, 0.118177742, 0.0674064159},
+	  .rotate = {0.551790953, 0.413088262, 0.45563665, -0.563272595},
+	  //.scale = {1.00009155, 1.00009155, 0.999816895}
+	  .scale = {1, 1, 1}
+	},
+	{ .translate = {-0.0412524939, -0.105564594, 0.852893293},
+	  .rotate = {-0.877423167, 0.216562629, 0.181933701, -0.387447715},
+	  //.scale = {0.999908447, 1.00018311, 0.999908447}
+	  .scale = {1, 1, 1}
+	},
+};
+
 int
 main ()
 {
@@ -114,5 +186,7 @@ main ()
 	for (size_t i = 0; i < countof (joints); i++) {
 		pass &= test_motor (joints[i]);
 	}
+	printf ("\n\n");
+	pass &= test_chain (chain_joints, countof (chain_joints));
 	return !pass;
 }
