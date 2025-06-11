6f3c39affa8bf8fe71bee90b901e0fe049a91e25
Bill Currie <bill@taniwha.org>
2025-06-11 12:09:46 +0900

    [qfcc] Add a failing test for inout pointers
    It turns out that inout (or maybe just out, probably doesn't matter)
    doesn't work too well when the function with an inout param is followed
    by another function call.

---
+++ tools/qfcc/test/ptrincinout2.r
@@ -0,0 +1,40 @@
+#include "test-harness.h"
+
+static int
+func (@inout void *ptr)
+{
+	auto p = (int *)ptr;
+	int v = *p++;
+	ptr = p;
+	return v;
+}
+
+int data[] = {1, 2, 3};
+int dest[countof(data)];
+
+void
+set_dest (int i, int v)
+{
+	dest[i] = v + 1;
+}
+
+int
+main ()
+{
+	int *ptr = data;
+	for (int i = 0; i < countof (data); i++) {
+		set_dest(i, func(ptr));
+	}
+	//printf ("ptr - data: %d\n", ptr - data);
+	bool fail = false;
+	//if (ptr - data != countof (data)) {
+	//	fail = true;
+	//}
+	for (int i = 0; i < countof (data); i++) {
+		printf ("%d %d %d\n", i, data[i], dest[i]);
+		if (dest[i] != data[i] + 1) {
+			fail = true;
+		}
+	}
+	return fail & 1;
+}
