4d6000cbbb6ee2e2ba60cdfc530ab728c04ca5fb
Bill Currie <bill@taniwha.org>
2025-06-16 20:45:05 +0900

    [vulkan] Update the debug macros for C23
    It might be slightly older, but __VA_OPT__ and __VA_ARGS__ is much nicer
    as the final fixed param isn't forced to carry the variable arguments.

--- include/QF/Vulkan/debug.h
+++ include/QF/Vulkan/debug.h
@@ -9,24 +9,25 @@
 #include "QF/simd/types.h"
 
 #if (defined(_WIN32) && !defined(_WIN64)) || (!defined(_WIN32) && __WORDSIZE < 64)
-#define QFV_duCmdBeginLabel(device, cmd, name...)
+#define QFV_duCmdBeginLabel(device, cmd, name, ...)
 #define QFV_duCmdEndLabel(device, cmd)
-#define QFV_duCmdInsertLabel(device, cmd, name...)
+#define QFV_duCmdInsertLabel(device, cmd, name, ...)
 #define QFV_duCreateMessenger(inst, severity, type, callback, data, messenger)
 #define QFV_duDestroyMessenger(inst, messenger)
-#define QFV_duQueueBeginLabel(device, queue, name...)
+#define QFV_duQueueBeginLabel(device, queue, name, ...)
 #define QFV_duQueueEndLabel(device, queue)
-#define QFV_duQueueInsertLabel(device, queue, name...)
+#define QFV_duQueueInsertLabel(device, queue, name, ...)
 #define QFV_duSetObjectName(device, type, handle, name) ((void)device, (void)type, (void)handle)
 #define QFV_duSetObjectTag(device, type, handle, name, size, tag)
 #define QFV_duSubmitMessage(inst, severity, types, data)
 #else
-#define QFV_duCmdBeginLabel(device, cmd, name...)\
+#define QFV_duCmdBeginLabel(device, cmd, name, ...)\
 	do { \
 		qfv_devfuncs_t *dfunc = device->funcs; \
 		if (dfunc->vkCmdBeginDebugUtilsLabelEXT) { \
 			VkDebugUtilsLabelEXT qfv_du_label = { \
 				VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, 0, name \
+				__VA_OPT__(,) __VA_ARGS__ \
 			}; \
 			dfunc->vkCmdBeginDebugUtilsLabelEXT (cmd, &qfv_du_label); \
 		} \
@@ -40,12 +41,13 @@
 		} \
 	} while (0)
 
-#define QFV_duCmdInsertLabel(device, cmd, name...) \
+#define QFV_duCmdInsertLabel(device, cmd, name, ...) \
 	do { \
 		qfv_devfuncs_t *dfunc = device->funcs; \
 		if (dfunc->vkCmdInsertDebugUtilsLabelEXT) { \
 			VkDebugUtilsLabelEXT qfv_du_label = { \
 				VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, 0, name \
+				__VA_OPT__(,) __VA_ARGS__ \
 			}; \
 			dfunc->vkCmdInsertDebugUtilsLabelEXT (cmd, &qfv_du_label); \
 		} \
@@ -70,12 +72,13 @@
 		} \
 	} while (0)
 
-#define QFV_duQueueBeginLabel(device, queue, name...) \
+#define QFV_duQueueBeginLabel(device, queue, name, ...) \
 	do { \
 		qfv_devfuncs_t *dfunc = device->funcs; \
 		if (dfunc->vkQueueBeginDebugUtilsLabelEXT) { \
 			VkDebugUtilsLabelEXT qfv_du_label = { \
 				VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, 0, name \
+				__VA_OPT__(,) __VA_ARGS__ \
 			}; \
 			dfunc->vkQueueBeginDebugUtilsLabelEXT (queue, &qfv_du_label); \
 		} \
@@ -89,12 +92,13 @@
 		} \
 	} while (0)
 
-#define QFV_duQueueInsertLabel(device, queue, name...) \
+#define QFV_duQueueInsertLabel(device, queue, name, ...) \
 	do { \
 		qfv_devfuncs_t *dfunc = device->funcs; \
 		if (dfunc->vkQueueInsertDebugUtilsLabelEXT) { \
 			VkDebugUtilsLabelEXT qfv_du_label = { \
 				VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, 0, name \
+				__VA_OPT__(,) __VA_ARGS__ \
 			}; \
 			dfunc->vkQueueInsertDebugUtilsLabelEXT (queue, &qfv_du_label); \
 		} \
