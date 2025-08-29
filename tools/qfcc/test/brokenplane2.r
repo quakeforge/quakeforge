1c6f85ef626b834b5561a6096025fc511098e2c6
Bill Currie <bill@taniwha.org>
2025-08-29 23:51:10 +0900

    [qfcc] Add a test for just the plane in brokenplane
    This is rather redundant, but it's a nice simple test that was fixed by
    394563270c48ed1e0d80536297d59a9b13c9edf8 after the previous commit broke
    both brokenplane and brokenkill.

---
+++ tools/qfcc/test/brokenplane2.r
@@ -0,0 +1,35 @@
+typedef @algebra(float(3,0,1)) PGA;
+typedef PGA.group_mask(0x0a) bivector_t;
+typedef PGA.group_mask(0x1e) motor_t;
+typedef PGA.tvec point_t;
+typedef PGA.vec plane_t;
+
+@overload float sqrt(float x) = #0;
+void printf (string fmt, ...) = #0;
+
+plane_t broken_plane (point_t eye, point_t target, bivector_t l)
+{
+	//printf ("e:%q\n", eye);
+	//printf ("t:%q\n", target);
+	@algebra (PGA) {
+		auto p = (eye ∨ target ∨ e021);
+		l /= sqrt (l • ~l);
+		p /= sqrt (p • p);
+		//printf ("l:%v %v\n", l.bvect, l.bvecp);
+		return p;
+	}
+}
+
+int
+main ()
+{
+	float x = sqrt(0.5f);
+	@algebra (PGA) {
+		point_t eye = -5 * e032 + (-3*x) * e013 + (1.5 + 3*x) * e021 + e123;
+		point_t target = -5 * e032 + 1.5 * e021 + e123;
+		auto l = -(eye ∨ target);
+		auto p = broken_plane (eye, target, l);
+		printf ("p:%q\n", p);
+		return (vec4)p != '1 0 0 5';
+	}
+}
