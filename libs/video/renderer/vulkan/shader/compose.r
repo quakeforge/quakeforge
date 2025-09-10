2f0d80e35ca060505cdfd5e96734bc22fdd67183
Bill Currie <bill@taniwha.org>
2025-09-10 13:22:53 +0900

    [vulkan] Add a comment about the device lost error
    I've spent a week chasing this problem and it's hard to say whether I've
    made any progress on it (though QF has improved in general due to the
    chase). I'm tired of it and want to move on to other things (which I can
    since starting flat seems to be reliable enough (if I've seen any
    lockups, it was once or twice over two years)), but leaving some notes
    seems like a good idea, especially regarding the apparent trigger. I'm
    pretty certain (though not entirely convinced) it's a sneaky (VVL misses
    it) synchronization error rather than a driver or hardware issue, though
    things did seem to work nicely on AMD when Charles tried it for me (but
    it was getting late and we were running into other issues with QF).

--- libs/video/renderer/vulkan/shader/compose.r
+++ libs/video/renderer/vulkan/shader/compose.r
@@ -31,6 +31,13 @@ main (void)
 	vec3        o = max(BlendFrags (vec4 (c * l + e, 1)).xyz, vec3(0));
 
 	float d = p.w > 0 ? length (p.xyz - camera.xyz) : 1e36;
+	//FIXME for some reason, doing fog blend randomly causes a device lost
+	//error when starting QF in fisheye mode, but everything seems to work
+	//reliably if fisheye is enabled after startup. However, I am fairly sure
+	//that is just a symptom and there's a problem with my synchronization that
+	//VVL doesn't catch and the combination of cubemap rendering (which fisheye
+	//uses) and calculating fog somehow triggers (I really hope it's not a
+	//driver (or worse, hardware) bug).
 	o = FogBlend (vec4(o,1), fog, d).xyz;
 	o = pow (o, vec3(0.83));//FIXME make gamma correction configurable
 	frag_color = vec4 (o, 1);
