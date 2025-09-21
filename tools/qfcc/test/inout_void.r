656c089fa7a1eecc40b3413dae7dec459207c268
Bill Currie <bill@taniwha.org>
2025-09-22 08:58:15 +0900

    [qfcc] Add a pair of failing inout tests
    I discovered the unused problem when debugging my gizmo shader in qdb,
    and the void problem while reproducing the unused bug. The void one
    turns out to be quite nasty in that the comparison doesn't even compile
    (ice) when optimizing.

---
+++ tools/qfcc/test/inout_void.r
@@ -0,0 +1,26 @@
+int ret_val;
+
+void draw_brush (int ind, vec3 v, vec3 eye, @inout vec4 color)
+{
+	if (ind != 0 || eye != '0 0 1' || v != '-1 1 0' || color != '1 1 1 1') {
+		ret_val = 1;
+		return;
+	}
+	color = '1 2 3 4';
+	ret_val = 0;
+}
+
+vec4 x;
+
+int main()
+{
+	auto eye = vec3(-0, 0, 1);
+	auto v = vec3(-1, 1, 0);
+	auto color = vec4 (1, 1, 1, 1);
+
+	draw_brush (0, v, eye, color);
+	if (color != '1 2 3 4') {
+		ret_val = 1;
+	}
+	return ret_val;
+}
