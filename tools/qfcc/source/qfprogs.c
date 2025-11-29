788d9dc623ccaa5d5c851ad26296a0205a123c46
Bill Currie <bill@taniwha.org>
2025-11-29 22:22:31 +0900

    [qfprogs] Clear progs memory when loading
    While debugging the missing local variables, valgrind had a thing or two
    to say.

--- tools/qfcc/source/qfprogs.c
+++ tools/qfcc/source/qfprogs.c
@@ -201,7 +201,7 @@ load_file (progs_t *pr, const char *name, off_t *_size)
 static void *
 allocate_progs_mem (progs_t *pr, int size)
 {
-	return malloc (size);
+	return calloc (1, size);
 }
 
 static void
