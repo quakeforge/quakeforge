d2d30a1037acc90c2be641ae9f84752b75960f18
Bill Currie <bill@taniwha.org>
2025-11-30 18:58:04 +0900

    [gamecode] Maybe optimize PR_RESGET
    If there actually was a performance gain, it was probably lost in the
    noise, but it's use very heavily in builtins, so any little gain can
    help. Unconditionally setting ret to nullptr did seem to hurt, so maybe
    avoiding any unnecessary assignments and the __builtin_expect do help.

--- include/QF/progs.h
+++ include/QF/progs.h
@@ -1886,13 +1886,13 @@ void *PR_Resources_Find (progs_t *pr, const char *name);
 */
 #define PR_RESGET(map,ind)										\
 	({															\
-		__auto_type ret = (map)._free;							\
+		typeof((map)._free) ret;								\
 		unsigned    row = ~ind / 1024;							\
 		unsigned    col = ~ind % 1024;							\
-		if (row >= (map)._size) {								\
-			ret = 0;											\
-		} else {												\
+		if (__builtin_expect (row < (map)._size, 1)) {			\
 			ret = &(map)._map[row][col];						\
+		} else {												\
+			ret = nullptr;										\
 		}														\
 		ret;													\
 	})
