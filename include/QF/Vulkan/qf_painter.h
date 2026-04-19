8d52fcd2c3aafdc0c51b64291ae1a246d32975fa
Bill Currie <bill@taniwha.org>
2026-04-20 08:53:38 +0900

    [vulkan] Move painter structs into the header file
    Again, for better organization and easier editing.

--- include/QF/Vulkan/qf_painter.h
+++ include/QF/Vulkan/qf_painter.h
@@ -29,8 +29,61 @@
 #define __QF_Vulkan_qf_painter_h
 
 #include "QF/simd/types.h"
+#include "QF/Vulkan/staging.h"
 
 typedef struct vulkan_ctx_s vulkan_ctx_t;
+
+typedef struct uip_cmd_s {
+	uint32_t    cmd;
+	uint32_t    next;
+} uip_cmd_t;
+
+typedef struct uip_line_s {
+	uip_cmd_t   cmd;
+	float       a[2];
+	float       b[2];
+	float       r;
+	byte        col[4];
+} uip_line_t;
+
+typedef struct uip_circle_s {
+	uip_cmd_t   cmd;
+	float       c[2];
+	float       r;
+	byte        col[4];
+} uip_circle_t;
+
+typedef struct uip_box_s {
+	uip_cmd_t   cmd;
+	float       c[2];
+	float       e[2];
+	float       r;
+	byte        col[4];
+} uip_box_t;
+
+typedef struct painterframe_s {
+	VkImage     cmd_heads_image;
+	VkImageView cmd_heads_view;
+	VkBuffer    cmd_queue;
+	VkDescriptorSet set;
+} painterframe_t;
+
+typedef struct painterframeset_s
+	DARRAY_TYPE (painterframe_t) painterframeset_t;
+
+typedef struct uip_queue_s
+	DARRAY_TYPE (uint32_t) uip_queue_t;
+
+typedef struct painterctx_s {
+	qfv_dsmanager_t *dsmanager;
+	qfv_resource_t *resource;
+	painterframeset_t frames;
+	qfv_extent_t cmd_extent;
+	uint32_t    max_queues;
+	uint32_t   *cmd_heads;
+	uint32_t   *cmd_tails;
+	uip_queue_t cmd_queue;
+} painterctx_t;
 
 void Vulkan_Painter_Init (vulkan_ctx_t *ctx);
 void Vulkan_Painter_AddLine (vec2f_t p1, vec2f_t p2, float r,
