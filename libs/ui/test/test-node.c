eed53cc8f859581304068b2a8de69eb9c9612d15
Bill Currie <bill@taniwha.org>
2026-04-22 13:19:39 +0900

    [ui] Improve node test a little
    It checks for link consistency and shows nicer names for the connection
    points.

--- libs/ui/test/test-node.c
+++ libs/ui/test/test-node.c
@@ -26,8 +26,10 @@ static node_link_t test_links[] = {
 	{ .src = 0, .dst = 1 },
 };
 
-static uint32_t out_connectors[3];
-static uint32_t in_connectors[3];
+static const char *out_names[] = { "a", "b", "c" };
+static const char *in_names[] = { "d", "e", "f" };
+static uint32_t out_connectors[countof(out_names)];
+static uint32_t in_connectors[countof(in_names)];
 static uint32_t links[countof (test_links)];
 
 static int
@@ -69,6 +71,8 @@ main (void)
 		out_connectors[i] = ECS_NewEntity (test_sys.reg);
 		uint32_t grp_id = ECS_NewSubpoolRange (test_sys.reg, t_link_out);
 		Ent_SetComponent (out_connectors[i], t_con_out, test_sys.reg, &grp_id);
+		Ent_SetComponent (out_connectors[i], ecs_name, test_sys.reg,
+						  &out_names[i]);
 	}
 
 	// create the set of input end-points
@@ -76,6 +80,8 @@ main (void)
 		in_connectors[i] = ECS_NewEntity (test_sys.reg);
 		uint32_t grp_id = ECS_NewSubpoolRange (test_sys.reg, t_link_in);
 		Ent_SetComponent (in_connectors[i], t_con_in, test_sys.reg, &grp_id);
+		Ent_SetComponent (in_connectors[i], ecs_name, test_sys.reg,
+						  &in_names[i]);
 	}
 
 	// create the links from output end-points to input end-points
@@ -95,12 +101,31 @@ main (void)
 
 	for (uint32_t i = 0; i < countof (links); i++) {
 		uint32_t l = links[i];
+		uint32_t *l_out = Ent_GetComponent (l, t_link_out, test_sys.reg);
+		uint32_t *l_in = Ent_GetComponent (l, t_link_in,  test_sys.reg);
 		node_link_t *con = Ent_GetComponent (l, t_link_con, test_sys.reg);
-		printf ("link: %d %3x.%05x out:%08x in:%08x src:%08x dst:%08x\n",
+		const char **snm = Ent_GetComponent (con->src, ecs_name, test_sys.reg);
+		const char **dnm = Ent_GetComponent (con->dst, ecs_name, test_sys.reg);
+		printf ("link: %d %3x.%05x"
+				" %sout:%08x"DFL
+				" %sin:%08x"DFL
+				" %ssrc:%08x"DFL
+				" %sdst:%08x"DFL
+				" %s%s->%s"DFL"\n",
 				i, Ent_Generation (l) >> ENT_IDBITS, Ent_Index (l),
-				*(uint32_t *) Ent_GetComponent (l, t_link_out, test_sys.reg),
-				*(uint32_t *) Ent_GetComponent (l, t_link_in,  test_sys.reg),
-				con->src, con->dst);
+				*l_out == con->src ? DFL : RED,
+				*l_out,
+				*l_in == con->dst ? DFL : RED,
+				*l_in,
+				*l_out == con->src ? DFL : RED,
+				con->src,
+				*l_in == con->dst ? DFL : RED,
+				con->dst,
+				*l_out == con->src && *l_in == con->dst ? GRN : RED,
+				*snm, *dnm);
+		if (*l_out != con->src || *l_in != con->dst) {
+			ret = 1;
+		}
 	}
 	auto subpools = test_sys.reg->subpools;
 	if (check_subpool_ranges (&subpools[test_sys.base + node_link_out],
