e7909bc1c16e456852b5523bb0d598b144f5819a
Bill Currie <bill@taniwha.org>
2026-06-08 13:40:04 +0900

    [vulkan] Fix some gcc impurity

--- include/QF/Vulkan/qf_texture.h
+++ include/QF/Vulkan/qf_texture.h
@@ -23,13 +23,13 @@ typedef struct texturectx_s {
 	uint32_t comp_base;
 } texturectx_t;
 
-VkImageView QFV_Tex_View (vulkan_ctx_t *ctx, uint32_t texid);
-VkSampler QFV_Tex_Sampler (vulkan_ctx_t *ctx, uint32_t texid);
+VkImageView QFV_Tex_View (vulkan_ctx_t *ctx, uint32_t texid) __attribute__((pure));
+VkSampler QFV_Tex_Sampler (vulkan_ctx_t *ctx, uint32_t texid) __attribute__((pure));
 uint32_t QFV_LoadTexinfo (vulkan_ctx_t *ctx, qfv_textureinfo_t *texinfo,
 						  const char *name);
-uint32_t QFV_TexGetSkinid (vulkan_ctx_t *ctx, uint32_t id);
+uint32_t QFV_TexGetSkinid (vulkan_ctx_t *ctx, uint32_t id) __attribute__((pure));
 void QFV_TexSetSkinid (vulkan_ctx_t *ctx, uint32_t id, uint32_t skinid);
-VkDescriptorSet QFV_GetTexture (vulkan_ctx_t *ctx, uint32_t texid);
+VkDescriptorSet QFV_GetTexture (vulkan_ctx_t *ctx, uint32_t texid) __attribute__((pure));
 
 void Vulkan_ExpandPalette (byte *dst, const byte *src, const byte *palette,
 						   int alpha, int count);
