5689ae54ec2aef98bdca6bcd4aa1126b2b4de2c5
Bill Currie <bill@taniwha.org>
2026-07-06 17:31:50 +0900

    [qfcc] Correct calcs for brokenbox1
    I had multiplied when I should have divided, but the test did still show
    the right thing, just not the right numbers. Now the numbers are right.

--- tools/qfcc/test/brokenbox1.r
+++ tools/qfcc/test/brokenbox1.r
@@ -10,7 +10,7 @@ mat2x3 box (vec3 e, float invDensity)
 	I[0] = '1 1 1';
 	I[1] = [e.y + e.z, e.z + e.x, e.x + e.y];// * i * invDensity;
 	I[0] = I[0] * invDensity;
-	I[1] = I[1] * i * invDensity;
+	I[1] = I[1] * invDensity / i;
 	return I;
 }
 
@@ -19,5 +19,5 @@ int main ()
 	vec3 ext = '1 4 9';
 	auto I = box(ext, 36*3*8);
 	printf ("%.9v %.9v\n", I[0], I[1]);
-	return (I[0] != '3 3 3' || I[1] != '97.0000076 82 17') & 1;
+	return (I[0] != '3 3 3' || I[1] != '873 738 153') & 1;
 }
